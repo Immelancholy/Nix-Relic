f714cf6d876ae81b0a2ce83ebee4927f0e9cdcb0
Immelancholy <lenalowes0@gmail.com>
2025-02-15 18:24:11 +0000

    binds

--- home/programs/hyprland/hyprconf/binds.nix
+++ home/programs/hyprland/hyprconf/binds.nix
@@ -17,7 +17,8 @@
       "$mod, I, exec, [workspace 1 silent; float; size 626 638; move 10 432]$term sh $scr/btop.sh"
       "$mod, I, exec, [workspace 1 silent; float; size 400 1032; move 1510 38]$term sh $scr/neo.sh"
       "$mod, I, exec, [workspace 1 silent; float; size 626 384; move 10 37]$term --hold fastfetch" 
-
+      "$mod, Delete, exec, uwsm stop";
+      "$mod+Shift, 0, movetoworkspace, 10"
     ]
     ++ (
         # workspaces
@@ -32,4 +33,60 @@
           9)
       );
   };
+  wayland.windowManager.hyprland.extraConfig = '' 
+    bindl  = , XF86AudioMute, exec, playerctl --player=mpd play-pause #pause-play media
+    bindel = , XF86AudioLowerVolume, exec, playerctl --player=mpd volume 0.01- # decrease volume
+    bindel = , XF86AudioRaiseVolume, exec, playerctl --player=mpd volume 0.01+ # increase volume
+    bindl  = , mouse:277, exec, playerctl --player=mpd play-pause # toggle between media play and pause
+    bindel  = , mouse:281, exec, playerctl --player=mpd volume 0.01+ # volume up
+    bindel  = , mouse:282, exec, playerctl --player=mpd volume 0.01- # volume down
+    bindl  = $mod, mouse:282, exec, playerctl --player=mpd next #next
+    bindl  = $mod, mouse:281, exec, playerctl --player=mpd previous #previous,
+    bindl = , mouse:275, pass, class:^(discord)$
+    bindl = , F10, pass, class:^(com.obsproject.Studio)$
+    bindl = , F12, pass, class:^(com.obsproject.Studio)$
+    bindl = , F9, pass, class:^(com.obsproject.Studio)$
+    bind = $mod, M, exec,[workspace 4 silent] /home/mela/scripts/patch.sh
+    bind = $mod, M, exec,[workspace 4 silent] qpwgraph
+
+    bind = $mod, H, movefocus, l
+    bind = $mod, L, movefocus, r
+    bind = $mod, K, movefocus, u
+    bind = $mod, J, movefocus, d
+    bind = Alt, Tab, movefocus, d
+
+    bind = $mod+Ctrl, H, workspace, r+1
+    bind = $mod+Ctrl, L, workspace, r-1
+
+    bind = $mod+Ctrl, J, workspace, empty 
+
+    binde = $mod+Shift, H, resizeactive, 1 0
+    binde = $mod+Shift, L, resizeactive, -1 0
+    binde = $mod+Shift, K, resizeactive, 0 -1
+    binde = $mod+Shift, J, resizeactive, 0 1
+
+    $moveactivewindow=grep -q "true" <<< $(hyprctl activewindow -j | jq -r .floating) && hyprctl dispatch moveactive
+    binded = $mod+Shift+Ctrl, H, Move activewindow left, exec, $moveactivewindow -30 0 || hyprctl dispatch movewindow l
+    binded = $mod+Shift+Ctrl, L, Move activewindow right, exec, $moveactivewindow 30 0 || hyprctl dispatch movewindow r
+    binded = $mod+Shift+Ctrl, K, Move activewindow up, exec, $moveactivewindow  0 -30 || hyprctl dispatch movewindow u
+    binded = $mod+Shift+Ctrl, J, Move activewindow down, exec, $moveactivewindow 0 30 || hyprctl dispatch movewindow d
+
+    bind = $mod, mouse_up, workspace, e+1
+    bind = $mod, mouse_down, workspace, e-1
+
+    bindm = $mod, mouse:272, movewindow
+    bindm = $mod, mouse:273, resizewindow
+    bindm = $mod, Z, movewindow
+    bindm = $mod, X, resizewindow
+
+    bind = $mod+Alt, S, movetoworkspacesilent, special
+    bind = $mod, S, togglespecialworkspace,
+
+    bind = $mod, R, togglesplit
+
+    bind = Ctrl+Shift, Escape, exec, $term --title "btop" sh $scr/btop.sh
+    
+    bindd = $mod+Shift, P,Color Picker , exec, hyprpicker -a # Pick color (Hex) >> clipboard# 
+
+  '';
 }
