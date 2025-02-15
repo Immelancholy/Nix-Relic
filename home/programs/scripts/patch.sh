e47284c5308ea56f6d61923f5cae4e7cc32797c8
Immelancholy <lenalowes0@gmail.com>
2025-02-15 21:34:45 +0000

    patchbay

--- home/programs/scripts/patch.sh
+++ home/programs/scripts/patch.sh
@@ -4,4 +4,4 @@ cd /home/mela/.config/rncbc.org/ || exit
 mv qpwgraph.conf qpwgraph_notuse1.conf
 mv qpwgraph_notuse.conf qpwgraph.conf
 mv qpwgraph_notuse1.conf qpwgraph_notuse.conf
-exec uwsm app -- /usr/bin/qpwgraph
+exec qpwgraph
