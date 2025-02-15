e2fc4549eb88b30cd08dfee7cdfefc757b83fd4d
Immelancholy <lenalowes0@gmail.com>
2025-02-15 17:48:36 +0000

    edit

--- home/programs/yazi.nix
+++ home/programs/yazi.nix
@@ -3,5 +3,9 @@
     enable = true;
     package = inputs.yazi.packages.${pkgs.system}.default;
     enableZshIntegration = true;
+    plugins = {};
+    settings = {
+      show_hidden = true;
+    };
   };
 }
