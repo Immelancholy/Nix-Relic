b925044321ec8a4773af53360644c230b0cd5484
Immelancholy <lenalowes0@gmail.com>
2025-02-15 17:34:29 +0000

    tpm

--- system/boot.nix
+++ system/boot.nix
@@ -1,6 +1,10 @@
 { pkgs, config, ... }: {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.systemd = {
+  	enable = true;
+	tpm2.enable = true;
+  };
   boot.extraModulePackages = with config.boot.kernelPackages; [ bbswitch ];
   boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.kernelParams = [
