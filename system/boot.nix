179f228c06589a1168fd62b4dad06349035aa387
Immelancholy <lenalowes0@gmail.com>
2025-02-15 14:28:33 +0000

    discord

--- system/boot.nix
+++ system/boot.nix
@@ -1,7 +1,7 @@
 { pkgs, config, ... }: {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.extraModulePackages = with config.boot.kernelPackages; [ bbswitch ];:qa
+  boot.extraModulePackages = with config.boot.kernelPackages; [ bbswitch ];
   boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.kernelParams = [
     "rw"
