6db0c0bafe08a04f2ae2a346a080116cfa22864c
Immelancholy <lenalowes0@gmail.com>
2025-02-15 20:49:19 +0000

    screenshot

--- home/default.nix
+++ home/default.nix
@@ -67,7 +67,7 @@
     nvim = {
       name = "Neovim";
       genericName = "Text Editor";
-      exec = "kitty sh /home/mela/.local/share/bin/kitty_nvim.sh %F";
+      exec = "kitty sh /home/mela/.local/share/bin/kitty_nvim.sh";
       type = "Application";
       terminal = false;
       categories = ["Utility" "TextEditor"];
