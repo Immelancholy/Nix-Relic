387ebe17f2f430fab495250b03f1c4c0f410dbd1
Immelancholy <lenalowes0@gmail.com>
2025-02-16 00:51:30 +0000

    hypr colours

--- home/programs/hyprland/hyprconf/animations.nix
+++ home/programs/hyprland/hyprconf/animations.nix
@@ -8,7 +8,7 @@
         bezier = wind, 0.05, 0.9, 0.1, 1.05
         bezier = winIn, 0.1, 1.1, 0.1, 1.1
         bezier = winOut, 0.3, -0.3, 0, 1
-        bezier = liner, 0.6, 1, 0.50, 0.40
+        bezier = liner, 0.6, 040, 0.50, 0.40
         animation = windows, 1, 6, wind, slide
         animation = windowsIn, 1, 6, winIn, slide
         animation = windowsOut, 1, 5, winOut, slide
