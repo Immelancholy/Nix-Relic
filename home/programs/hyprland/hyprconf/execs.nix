6a0b6f17c1bf780e6be777b9723e565074969f53
Immelancholy <lenalowes0@gmail.com>
2025-02-16 17:12:24 +0000

    nvim

--- home/programs/hyprland/hyprconf/execs.nix
+++ home/programs/hyprland/hyprconf/execs.nix
@@ -5,11 +5,11 @@
     ];
     exec-once = [
       "$scr/swww.sh"
-      "[workspace 1 silent; float; size 854 557; move 646 38]$term sh $scr/rmpc.sh"
-      "[workspace 1 silent; float; size 854 465; move 646 605]$term sh $scr/cava.sh"
-      "[workspace 1 silent; float; size 626 638; move 10 432]$term sh $scr/btop.sh"
-      "[workspace 1 silent; float; size 400 1032; move 1510 38]$term sh $scr/neo.sh"
-      "[workspace 1 silent; float; size 626 384; move 10 37]$term --hold fastfetch"
+      "[workspace 1 silent; float; size 854 557; move 646 38] $term sh $scr/rmpc.sh"
+      "[workspace 1 silent; float; size 854 465; move 646 605] $term sh $scr/cava.sh"
+      "[workspace 1 silent; float; size 626 638; move 10 432] $term sh $scr/btop.sh"
+      "[workspace 1 silent; float; size 400 1032; move 1510 38] $term sh $scr/neo.sh"
+      "[workspace 1 silent; float; size 626 384; move 10 38] $term --hold fastfetch"
       "[workspace 2 silent] $browser"
       "[workspace 2 silent] teams-for-linux --minimized"
       "[workspace 2 silent] mullvad-vpn"
