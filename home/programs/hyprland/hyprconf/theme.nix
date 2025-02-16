191b7be7913aa7a6dc559fdc54e8344d0ece1a43
Immelancholy <lenalowes0@gmail.com>
2025-02-16 01:42:53 +0000

    hypr colours

--- home/programs/hyprland/hyprconf/theme.nix
+++ home/programs/hyprland/hyprconf/theme.nix
@@ -4,15 +4,15 @@
       gaps_in = 3
       gaps_out = 8
       border_size = 2
-      col.active_border = rgba(cba6f7ff) rgba(89dcebff) rgba(f5e0dcff) 45deg
+      col.active_border = rgba(cba6f7ff) rgba(89dcebff) rgba(f5e0dcff) 60deg
       col.inactive_border = rgba(b4befecc) rgba(6c7086cc) 45deg
       layout = dwindle
       resize_on_border = true
     }
     group {
-        col.border_active = rgba(cba6f7ff) rgba(89dcebff) rgba(f5e0dcff) 45deg
+        col.border_active = rgba(cba6f7ff) rgba(89dcebff) rgba(f5e0dcff) 60deg
         col.border_inactive = rgba(b4befecc) rgba(6c7086cc) 45deg
-        col.border_locked_active =rgba(cba6f7ff) rgba(89dcebff) rgba(f5e0dcff) 45deg
+        col.border_locked_active =rgba(cba6f7ff) rgba(89dcebff) rgba(f5e0dcff) 60deg
         col.border_locked_inactive = rgba(b4befecc) rgba(6c7086cc) 45deg
     }
 
