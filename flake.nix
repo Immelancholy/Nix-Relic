a2821b1092874eb5e359168eb75df337468f4853
Immelancholy <lenalowes0@gmail.com>
2025-02-16 18:25:24 +0000

    nvim

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
     nixosConfigurations = { 
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = {inherit inputs;};
+        specialArgs = {inherit inputs; inherit self;};
         modules = [
 	  solaar.nixosModules.default
           ./system
