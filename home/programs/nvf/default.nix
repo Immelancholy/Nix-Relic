c8eff70e0cde00f4c5e71c0440295c5ccb4507de
Immelancholy <lenalowes0@gmail.com>
2025-02-16 17:48:45 +0000

    nvim

--- home/programs/nvf/default.nix
+++ home/programs/nvf/default.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... }: {
+{ pkgs, inputs, ... }: {
   programs.nvf = {
     enable = true;
     # your settings need to go into the settings attribute set
     # most settings are documented in the appendix
     settings = {
       vim = {
-        package = pkgs.neovim-unwrapped;
+        package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
         viAlias = false;
         vimAlias = true;
         lsp = {
