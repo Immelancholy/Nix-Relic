73f786d72d06e6cb111fda20d62964e3e08d6770
Immelancholy <lenalowes0@gmail.com>
2025-02-17 19:42:30 +0000

    waybar

--- home/programs/waybar/waybarsettings.nix
+++ home/programs/waybar/waybarsettings.nix
@@ -196,7 +196,7 @@
         tooltip = false;
       };
       "cava" = {
-        cava_config = "$XDG_CONFIG_HOME/cava/config.conf}";
+        cava_config = "$XDG_CONFIG_HOME/cava/config";
         framerate = 144;
         sensitivity = 100;
         bars = 8;
