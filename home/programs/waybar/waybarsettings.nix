821d861abafcf5749675638ea0b32995875c2a74
Immelancholy <lenalowes0@gmail.com>
2025-02-17 11:31:44 +0000

    hypr

--- home/programs/waybar/waybarsettings.nix
+++ home/programs/waybar/waybarsettings.nix
@@ -19,6 +19,7 @@
         "clock"
       ];
       modules-right = [
+        "mpd"
         "cpu"
         "memory"
         "pulseaudio"
@@ -27,6 +28,7 @@
         "keyboard-state"
         "custom/power"
       ];
+
       "hyprland/workspaces" = {
         all-outputs = true;
         active-only = false;
@@ -36,6 +38,53 @@
         on-scroll-down = "hyprctl dispatch workspace +1";
         persistent-workspaces = {
         };
+      };
+
+      "keyboard-state" = {
+        numlock = true;
+        capslock = true;
+        format = "{name} {icon}";
+        format-icons = {
+          locked = "";
+          unlocked = "";
+        };
+      };
+
+      "mpd" = {
+        format = "{stateIcon} {consumeIcon}{randomIcon}{repeatIcon}{singleIcon}{artist} - {album} - {title} ({elapsedTime:%M:%S}/{totalTime:%M:%S}) ⸨{songPosition}|{queueLength}⸩ {volume}% ";
+        format-disconnected = "Disconnected ";
+        format-stopped = "{consumeIcon}{randomIcon}{repeatIcon}{singleIcon}Stopped ";
+        unknown-tag = "N/A";
+        interval = 5;
+        consume-icons = {
+          on = " ";
+        };
+        "random-icons" = {
+          off = "<span color=\"#f53c3c\"></span> ";
+          on = " "; 
+        };
+        "repeat-icons" = {
+          on = " ";
+        };
+        "single-icons" = {
+          on = "1 ";
+        };
+        "state-icons" = {
+          paused = "";
+          playing = "";
+        };
+        tooltip-format = "MPD (connected)";
+        tooltip-format-disconnected = "MPD (disconnected)";
+      };
+      "idle_inhibitor" = {
+        format = "{icon}";
+        format-icons = {
+          activated = "";
+          deactivated = "";
+        };
+      };
+      tray = {
+        spacing = 10;
       };
     };
   };
