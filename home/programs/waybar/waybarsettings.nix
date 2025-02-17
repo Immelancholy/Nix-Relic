bfc9f597492c8eb9fb9cbf0544715fc8c54a05b8
Immelancholy <lenalowes0@gmail.com>
2025-02-17 03:04:53 +0000

    waybar

--- home/programs/waybar/waybarsettings.nix
+++ home/programs/waybar/waybarsettings.nix
@@ -2,8 +2,6 @@
   programs.waybar.settings = {
     mainBar = {
       layer = "top";
-      margin-top = 14;
-      margin-bottom = 14;
       
       modules-left = [
         "hyprland/workspaces"
