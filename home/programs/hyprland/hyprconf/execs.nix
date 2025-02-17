90736395a4e31049d9e8afa706b8b21c95fb9b6f
Immelancholy <lenalowes0@gmail.com>
2025-02-17 11:02:26 +0000

    hypr

--- home/programs/hyprland/hyprconf/execs.nix
+++ home/programs/hyprland/hyprconf/execs.nix
@@ -2,8 +2,9 @@
   wayland.windowManager.hyprland.settings = {
     exec-once = [
       "dbus-update-activation-environment --systemd --all"
-      "sleep 1; $scr/swww.sh"
-      "[workspace 2 silent] sleep 1; mullvad-vpn"
+      "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+      "$scr/swww.sh"
+      "[workspace 2 silent] mullvad-vpn"
       "[workspace 1 silent; float; size 854 557; move 646 38] $term sh $scr/rmpc.sh"
       "[workspace 1 silent; float; size 854 465; move 646 605] $term sh $scr/cava.sh"
       "[workspace 1 silent; float; size 626 638; move 10 432] $term sh $scr/btop.sh"
@@ -12,7 +13,7 @@
       "[workspace 4 silent] qpwgraph"
       "[workspace 2 silent] $browser"
       "[workspace 2 silent] teams-for-linux --minimized"
-      "[workspace 3] sleep 2; discord"
+      "[workspace 3] sleep 3; discord"
       "[workspace 5 silent] easyeffects"
     ];
   };
