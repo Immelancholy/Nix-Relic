76a8e0e140eb06e77515be97fa2f6b4fa3b4161b
Immelancholy <lenalowes0@gmail.com>
2025-02-17 23:39:26 +0000

    discord

--- home/default.nix
+++ home/default.nix
@@ -55,7 +55,7 @@
       enable = false;
     };
   };
-  xdg.portal = lib.mkDefault {
+  xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
     configPackages = with pkgs; [xdg-desktop-portal-gtk ];
