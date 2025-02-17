da268a2dbf2eea7a00553b906fd0f4138e26b9de
Immelancholy <lenalowes0@gmail.com>
2025-02-17 17:21:35 +0000

    update

--- system/programs/obs.nix
+++ system/programs/obs.nix
@@ -5,7 +5,6 @@
     plugins = with pkgs; [
       obs-studio-plugins.obs-vkcapture
       obs-studio-plugins.obs-vaapi
-      obs-studio-plugins.obs-nvfbc
       obs-studio-plugins.obs-pipewire-audio-capture
     ];
   };
