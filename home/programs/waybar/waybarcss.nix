b8c484af37a6d28db0b8eb77a7d4c339f4931dc4
Immelancholy <lenalowes0@gmail.com>
2025-02-17 17:08:51 +0000

    hypr

--- home/programs/waybar/waybarcss.nix
+++ home/programs/waybar/waybarcss.nix
@@ -40,7 +40,7 @@ tooltip {
 }
 
 #workspaces button.active {
-    background: alpha(@mauve, 0.95);
+    background: alpha(@mauve, 0.92);
     color: @surface0;
     margin-left: 3px;
     padding-left: 12px;
@@ -51,7 +51,7 @@ tooltip {
 }
 
 #workspaces button:hover {
-    background: alpha(@sky, 0.95);
+    background: alpha(@sky, 0.92);
     color: @surface0;
     animation: ws_hover 20s ease-in-out 1;
     transition: all 0.3s cubic-bezier(.55, -0.68, .48, 1.682);
