60d633b04b5d24a3fe22cacff21795a3f639ba0b
Immelancholy <lenalowes0@gmail.com>
2025-02-17 18:44:34 +0000

    waybar

--- home/programs/waybar/waybarcss.nix
+++ home/programs/waybar/waybarcss.nix
@@ -57,39 +57,6 @@ tooltip {
     transition: all 0.3s cubic-bezier(.55, -0.68, .48, 1.682);
 }
 
-#taskbar button {
-    box-shadow: none;
-    text-shadow: none;
-    padding: 0px;
-    border-radius: 9px;
-    margin-top: 3px;
-    margin-bottom: 3px;
-    margin-left: 0px;
-    padding-left: 3px;
-    padding-right: 3px;
-    margin-right: 0px;
-    color: @text;
-    animation: tb_normal 20s ease-in-out 1;
-}
-
-#taskbar button.active {
-    background: @subtext0;
-    color: @surface0;
-    margin-left: 3px;
-    padding-left: 12px;
-    padding-right: 12px;
-    margin-right: 3px;
-    animation: tb_active 20s ease-in-out 1;
-    transition: all 0.4s cubic-bezier(.55, -0.68, .48, 1.682);
-}
-
-#taskbar button:hover {
-    background: @mauve;
-    color: @surface0;
-    animation: tb_hover 20s ease-in-out 1;
-    transition: all 0.3s cubic-bezier(.55, -0.68, .48, 1.682);
-}
-
 #tray menu * {
     min-height: 16px
 }
