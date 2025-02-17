018055e7460917aab97e52e5adb46cf9bb0a6267
Immelancholy <lenalowes0@gmail.com>
2025-02-17 11:46:59 +0000

    hypr

--- home/programs/waybar/waybarsettings.nix
+++ home/programs/waybar/waybarsettings.nix
@@ -19,7 +19,6 @@
         "clock"
       ];
       modules-right = [
-        "mpd"
         "cpu"
         "temperature"
         "memory"
@@ -40,7 +39,6 @@
         persistent-workspaces = {
         };
       };
-
       "keyboard-state" = {
         numlock = true;
         capslock = true;
@@ -49,33 +47,6 @@
           locked = "";
           unlocked = "";
         };
-      };
-
-      "mpd" = {
-        format = "{stateIcon} {consumeIcon}{randomIcon}{repeatIcon}{singleIcon}{artist} - {album} - {title} ({elapsedTime:%M:%S}/{totalTime:%M:%S}) ⸨{songPosition}|{queueLength}⸩ {volume}% ";
-        format-disconnected = "Disconnected ";
-        format-stopped = "{consumeIcon}{randomIcon}{repeatIcon}{singleIcon}Stopped ";
-        unknown-tag = "N/A";
-        interval = 5;
-        consume-icons = {
-          on = " ";
-        };
-        "random-icons" = {
-          off = "<span color=\"#f53c3c\"></span> ";
-          on = " "; 
-        };
-        "repeat-icons" = {
-          on = " ";
-        };
-        "single-icons" = {
-          on = "1 ";
-        };
-        "state-icons" = {
-          paused = "";
-          playing = "";
-        };
-        tooltip-format = "MPD (connected)";
-        tooltip-format-disconnected = "MPD (disconnected)";
       };
       "idle_inhibitor" = {
         format = "{icon}";
@@ -93,7 +64,6 @@
       };
       "cpu" = {
         format = "{usage}%";
-        tooltip = false;
       };
       "temperature" = {
         critical-threshold = 80;
@@ -103,6 +73,14 @@
           "" 
           ""
         ];
+      };
+      "network" = {
+        format-wifi = "{essid} ({signalStrength}%) ";
+        format-ethernet = "{ipaddr}/{cidr} 󰊗";
+        tooltip-format = "{ifname} via {gwaddr} 󰈁";
+        format-linked = "{ifname} (No IP) 󰈂";
+        format-disconnected = "Disconnected 󰖪";
+        format-alt = "{ifname}: {ipaddr}/{cidr}";
       };
     };
   };
