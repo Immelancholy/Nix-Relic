1bd7312df63f9c3585fbac95f9b316568de14c4f
Immelancholy <lenalowes0@gmail.com>
2025-02-18 13:34:55 +0000

    nvim

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -1,6 +1,12 @@
 { pkgs, inputs, ... }: {
 	programs.nixvim = {
 		enable = true;
+		colorscheme = "catppuccin-mocha";
+		plugins = {
+			which-key.enable = true;
+			snacks.enable = true;
+			blink-cmp.enable = true;
+		};
 		extraPlugins = with pkgs.vimPlugins; [
 			plenary-nvim
 		];
