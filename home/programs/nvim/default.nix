49bff8b56d8588bd326abda3b0b9caeb64f1dbe5
Immelancholy <lenalowes0@gmail.com>
2025-02-18 17:16:34 +0000

    nvim

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -60,15 +60,32 @@
         enable = true;
         autoEnableSources = true;
         settings = {
-          __raw = ''
-            cmp.config.sources({
-              { name = 'nvim_lsp' },
-              { name = 'luasnip' },
-              { name = 'nvim_lua' },
-            }, {
-              { name = 'buffer' },
-            })
-          '';
+          mapping = {
+            __raw = ''
+              cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+              })
+            '';
+          };
+          snippet = {
+            expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+          };
+          source = {
+            __raw = ''
+              cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+                { name = 'nvim_lua' },
+                
+              }, {
+                { name = 'buffer' },
+              })
+            '';
+          };
         };
       };
       cmp-async-path.enable = true;
