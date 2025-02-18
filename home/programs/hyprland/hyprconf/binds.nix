b45a5c26577d1e43526b9273c0cb52678045ef07
Immelancholy <lenalowes0@gmail.com>
2025-02-18 11:32:48 +0000

    hyprtasking

--- home/programs/hyprland/hyprconf/binds.nix
+++ home/programs/hyprland/hyprconf/binds.nix
@@ -111,7 +111,7 @@
             layout = grid
 
             gap_size = 20
-            bg_color = 0xb4befe
+            bg_color = 0x1e1e2e
             border_size = 2
             exit_behavior = active interacted original hovered
 
