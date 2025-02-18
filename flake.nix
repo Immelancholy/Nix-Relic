55fbe21d0a8e5ca84036dd412f02390744d89a35
Immelancholy <lenalowes0@gmail.com>
2025-02-18 10:37:14 +0000

    discord

--- flake.nix
+++ flake.nix
@@ -26,6 +26,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    hyprland.url = "github:hyprwm/Hyprland";
   };
 
 
--- system/extracache.nix
+++ system/extracache.nix
@@ -2,5 +2,7 @@
   nix.settings = {
     extra-substituters = [ "https://yazi.cachix.org" ];
     extra-trusted-public-keys = [ "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k=" ];
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
 }
