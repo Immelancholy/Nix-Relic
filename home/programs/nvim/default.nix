24ca1083e4f0f9e85937992a042c4385504f7397
Immelancholy <lenalowes0@gmail.com>
2025-02-18 23:21:17 +0000

    nvm

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -303,41 +303,7 @@
             nix = [
               "nixfmt"
             ];
-          };
-          format_on_save = # Lua
-            ''
-              function(bufnr)
-                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                  return
-                end
-
-                if slow_format_filetypes[vim.bo[bufnr].filetype] then
-                  return
-                end
-
-                local function on_format(err)
-                  if err and err:match("timeout$") then
-                    slow_format_filetypes[vim.bo[bufnr].filetype] = true
-                  end
-                end
-
-                return { timeout_ms = 200, lsp_fallback = true }, on_format
-               end
-            '';
-          format_after_save = # Lua
-            ''
-              function(bufnr)
-                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                  return
-                end
-
-                if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-                  return
-                end
-
-                return { lsp_fallback = true }
-              end
-            '';
+          }; 
           log_level = "warn";
           notify_on_error = false;
           notify_no_formatters = false;
@@ -373,14 +339,7 @@
         mode = ["n"];
       }
       {
-        action = # Lua
-          ''
-            if vim.bo.filetype == "Dashboard" then
-              vim.cmd('qa')
-            else
-              vim.cmd('Dashboard')
-            end
-          '';
+        action = "<Cmd>Dashboard<CR>";
         key = "<leader>q";
         options.desc = "Return to Dashboard";
         mode = ["n"];
