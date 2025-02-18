4a0ad56ca28f75d9a377172cf21921db67ad1058
Immelancholy <lenalowes0@gmail.com>
2025-02-18 17:29:40 +0000

    nvim

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -71,23 +71,20 @@
               })
             '';
           };
-          sources = {
-            __raw = ''
-              require'cmp'.setup {
-                sources = {
-                  { name = 'nvim_lsp' }
-                }
-              }
-
-              -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-              local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-              -- An example for configuring `clangd` LSP to use nvim-cmp as a completion engine
-              require('lspconfig').clangd.setup {
-                capabilities = capabilities,
-              }
-            '';
-          };
+          sources = [
+            {
+              name = "nvim_lsp";
+            }
+            {
+              name = "luasnip";
+            }
+            {
+              name = "path";
+            }
+            {
+              name = "buffer";
+            }
+          ];
         };
       };
       cmp-async-path.enable = true;
