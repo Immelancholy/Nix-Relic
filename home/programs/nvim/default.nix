9ec3f0ba10bf15fbc7ce03a1d9ebdd6e63775566
Immelancholy <lenalowes0@gmail.com>
2025-02-18 23:31:00 +0000

    conform

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -301,50 +301,30 @@
         settings = {
           formatters_by_ft = {
             nix = [
-              "nixfmt"
+              "alejandra"
             ];
           };
-          format_on_save = # Lua
-            ''
-              function(bufnr)
-                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                  return
-                end
-
-                if slow_format_filetypes[vim.bo[bufnr].filetype] then
-                  return
-                end
-
-                local function on_format(err)
-                  if err and err:match("timeout$") then
-                    slow_format_filetypes[vim.bo[bufnr].filetype] = true
-                  end
-                end
-
-                return { timeout_ms = 200, lsp_fallback = true }, on_format
-               end
-            '';
-          format_after_save = # Lua
-            ''
-              function(bufnr)
-                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                  return
-                end
-
-                if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-                  return
-                end
-
-                return { lsp_fallback = true }
+          format_on_save = # Lua 
+          ''
+            function(bufnr)
+              local ignore_filetypes = { "sql", "jave" }
+              if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+                return
               end
-            '';
+              if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                return
+              end
+              local bufname = vim.api.nvim_buf_get_name(bufnr)
+              if bufname:match("/node_modules/") then
+                return
+              end
+              -- ...additional logic...
+              return { timeout_ms = 500, lsp_format = "fallback" }
+            end,
+          '';
           log_level = "warn";
           notify_on_error = false;
           notify_no_formatters = false;
-          formatters = {
-            nixfmt = {
-              command = lib.getExe pkgs.nixfmt-rfc-style;
-            };
           };
         };
       };
