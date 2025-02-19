937ac741ce427bd7fa6999299c9aedf538e3c881
Immelancholy <lenalowes0@gmail.com>
2025-02-19 00:25:45 +0000

    nvim

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -68,7 +68,32 @@
               {
                 __unkeyed-1 = "<leader>xx";
                 __unkeyed-2 = "<Cmd>Trouble diagnostics toggle<CR>";
-                desc = "Toggle diagnostics";
+                desc = "Diagnostics (Trouble)";
+              }
+              {
+                __unkeyed-1 = "<leader>xX";
+                __unkeyed-2 = "<cmd>Trouble diagnostics toggle filter.buf=0<cr>";
+                desc = "Buffer Diagnostics (Trouble)";
+              }
+              {
+                __unkeyed-1 = "<leader>cs";
+                __unkeyed-2 = "<cmd>Trouble symbols toggle focus=false<cr>";
+                desc = "Symbols (Trouble)";
+              }
+              {
+                __unkeyed-1 = "<leader>cl";
+                __unkeyed-2 = "<cmd>Trouble lsp toggle focus=false win.position=right<cr>";
+                desc = "LSP Definitions / references / ... (Trouble)";
+              }
+              {
+                __unkeyed-1 = "<leader>xL";
+                __unkeyed-2 = "<cmd>Trouble loclist toggle<cr>";
+                desc = "Location List (Trouble)";
+              }
+              {
+                __unkeyed-1 = "<leader>xQ";
+                __unkeyed-2 = "<cmd>Trouble qflist toggle<cr>";
+                desc = "Quickfix List (Trouble)";
               }
             ];
           }
