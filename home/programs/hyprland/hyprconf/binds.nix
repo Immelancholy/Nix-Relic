944c3a16c263144738da4fcfb3e2ed87f3a3354b
Immelancholy <lenalowes0@gmail.com>
2025-02-19 22:11:03 +0000

    :3

--- home/programs/hyprland/hyprconf/binds.nix
+++ home/programs/hyprland/hyprconf/binds.nix
@@ -40,11 +40,11 @@
   };
   wayland.windowManager.hyprland.extraConfig = ''
     bindl  = , XF86AudioMute, exec, playerctl --player=mpd play-pause #pause-play media
-    bindel = , XF86AudioLowerVolume, exec, playerctl --player=mpd volume 0.01- # decrease volume
-    bindel = , XF86AudioRaiseVolume, exec, playerctl --player=mpd volume 0.01+ # increase volume
+    bindel = , XF86AudioLowerVolume, exec, rmpc volume -1 # decrease volume
+    bindel = , XF86AudioRaiseVolume, exec, rmpc volume +1 # increase volume
     bindl  = , mouse:277, exec, playerctl --player=mpd play-pause # toggle between media play and pause
-    bindel  = , mouse:281, exec, playerctl --player=mpd volume 0.01+ # volume up
-    bindel  = , mouse:282, exec, playerctl --player=mpd volume 0.01- # volume down
+    bindel  = , mouse:281, exec, rmpc volume =1 # volume up
+    bindel  = , mouse:282, exec, rmpc volume -1 # volume down
     bindl  = $mod, mouse:282, exec, playerctl --player=mpd next #next
     bindl  = $mod, mouse:281, exec, playerctl --player=mpd previous #previous,
     bindl = , mouse:275, pass, class:^(discord)$
