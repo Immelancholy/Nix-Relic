c1acb667b0033c76b9df8bbe2f1e3b51dd97d511
Immelancholy <lenalowes0@gmail.com>
2025-02-19 21:41:08 +0000

    :3

--- home/qtk.nix
+++ home/qtk.nix
@@ -18,10 +18,7 @@
   home.pointerCursor = {
     gtk.enable = true;
     x11.enable = true;
-    hyprcursor = {
-      enable = true;
-      size = 20;
-    };
+    hyprcursor.enable = true;
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Ice";
     size = 20;
@@ -39,6 +36,8 @@
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
+      cursor-theme = "Bibata-Modern-Ice";
+      cursor-size = 20;
     };
   };
 }
