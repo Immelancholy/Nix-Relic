552d4e0d37031c35fae9295084f665058e8b675c
Immelancholy <lenalowes0@gmail.com>
2025-02-19 13:06:49 +0000

    electron

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,5 +1,9 @@
-{ pkgs, config, lib, ... }: {
-
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
   boot.blacklistedKernelModules = ["nouveau"];
   environment.systemPackages = [
     pkgs.nvtopPackages.full # nvtop
@@ -10,12 +14,14 @@
     pkgs.driversi686Linux.vdpauinfo
   ];
 
-  services.xserver.videoDrivers = [ "nvidia" ];
-    
+  boot.initrd.kernelModules = ["nvidia"];
+
+  services.xserver.videoDrivers = ["nvidia"];
+
   hardware.graphics = {
-  	enable = true;
+    enable = true;
     package = pkgs.mesa.drivers;
-  	enable32Bit = true;
+    enable32Bit = true;
     package32 = pkgs.pkgsi686Linux.mesa.drivers;
     extraPackages = with pkgs; [
       libvdpau-va-gl
@@ -31,14 +37,14 @@
   hardware.nvidia = {
     # Modesetting is required.
     modesetting.enable = true;
-      
+
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     powerManagement.enable = false;
-      
+
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
-      
+
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
     # Support is limited to the Turing and later architectures. Full list of
@@ -47,12 +53,12 @@
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
     open = false;
-      
+
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
-      
+
     nvidiaSettings = true;
-      
+
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
