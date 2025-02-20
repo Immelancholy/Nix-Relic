3b6d6ba8db1b75deb6a096540677e3a16c487057
Immelancholy <lenalowes0@gmail.com>
2025-02-20 17:03:05 +0000

    hypr

--- home/programs/hyprland/hyprconf/binds.nix
+++ home/programs/hyprland/hyprconf/binds.nix
@@ -68,8 +68,8 @@
     bind = $mod, Up, movefocus, u
     bind = $mod, Down, movefocus, d
 
-    # bind = Alt, L, workspace, r+1
-    # bind = Alt, H, workspace, r-1
+    bind = Alt, L, workspace, r+1
+    bind = Alt, H, workspace, r-1
     bind = Alt, Right, workspace, r+1
     bind = Alt, Left, workspace, r-1
 
