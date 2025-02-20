e1b4caa99d9f67fa7cec7b4ea1cd692f8b890fb6
Immelancholy <lenalowes0@gmail.com>
2025-02-20 02:58:58 +0000

    better format

--- home/programs/mpd/default.nix
+++ home/programs/mpd/default.nix
@@ -1,35 +1,37 @@
-{  
-  services.mpdris2 = {
-    enable = true;
-    mpd.host = "/run/user/1000/mpd/socket";
-    notifications = true;
-    multimediaKeys = true;
-    mpd.musicDirectory = "/home/mela/Music";
-  };
-  services.mpd = {
-    enable = true;
-    network = {
-      startWhenNeeded = true;
-      listenAddress = "/run/user/1000/mpd/socket";
+{
+  services = {
+    mpd = {
+      enable = true;
+      network = {
+        listenAddress = "/run/user/1000/mpd/socket";
+      };
+      musicDirectory = "~/Music";
+      extraConfig = ''
+        restore_paused "yes"
+
+        metadata_to_use	"artist,albumartist,album,title,track,name,genre,date,composer,performer,disc"
+        auto_update 	"yes"
+        auto_update_depth "4"
+        follow_outside_symlinks	"yes"
+        follow_inside_symlinks		"yes"
+        audio_output {
+          type  "pipewire"
+          name  "Pipewire Sound Server"
+          target  "alsa_output.usb-Audient_Audient_iD4-00.pro-output-0"
+        }
+        audio_output {
+          type  "pipewire"
+          name  "Pipewire CAVA"
+          target  "virtual_cable"
+        }
+      '';
     };
-    musicDirectory = "~/Music";
-    extraConfig = ''
-      restore_paused "yes"
-      metadata_to_use	"artist,albumartist,album,title,track,name,genre,date,composer,performer,disc"
-      auto_update 	"yes"
-      auto_update_depth "4"
-      follow_outside_symlinks	"yes"
-      follow_inside_symlinks		"yes"
-      audio_output {
-        type  "pipewire"
-        name  "Pipewire Sound Server"
-	target  "alsa_output.usb-Audient_Audient_iD4-00.pro-output-0"
-      }
-      audio_output {
-        type  "pipewire"
-        name  "Pipewire CAVA"
-	target  "virtual_cable"
-      }
-    ''; 
+    mpdris2 = {
+      enable = true;
+      mpd.host = "/run/user/1000/mpd/socket";
+      notifications = true;
+      multimediaKeys = true;
+      mpd.musicDirectory = "/home/mela/Music";
+    };
   };
 }
