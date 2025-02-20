aa0234f55ea8d6fc40b9a15afe4a7a97ab8dcb45
Immelancholy <lenalowes0@gmail.com>
2025-02-20 03:27:00 +0000

    better format

--- home/programs/mpd/default.nix
+++ home/programs/mpd/default.nix
@@ -5,7 +5,7 @@
       network = {
         listenAddress = "/run/user/1000/mpd/socket";
       };
-      musicDirectory = "$HOME/Music";
+      musicDirectory = "~/Music";
       extraConfig = ''
         restore_paused "yes"
 
@@ -30,7 +30,7 @@
       enable = true;
       mpd.host = "/run/user/1000/mpd/socket";
       notifications = true;
-      mpd.musicDirectory = "$HOME/Music";
+      mpd.musicDirectory = "~/Music";
     };
   };
 }
