fc5da38c3dbe9e49e00608ebaa687f763b3a716f
Immelancholy <lenalowes0@gmail.com>
2025-02-20 12:28:22 +0000

    blink-cmp

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -48,9 +48,9 @@
           sources = {
             default = [
               "lsp"
-              "buffer"
-              "path"
-              "luasnip"
+              # "buffer"
+              # "path"
+              # "luasnip"
             ];
             providers = {
               lsp = {
@@ -58,22 +58,22 @@
                 module = "blink.cmp.sources.lsp";
                 enabled = true;
               };
-              buffer = {
-                name = "buffer";
-                module = "blink.cmp.sources.buffer";
-                enabled = true;
-              };
-              path = {
-                name = "path";
-                modules = "blink.cmp.sources.path";
-                enabled = true;
-                async = true;
-              };
-              luasnip = {
-                name = "luasnip";
-                module = "blink.cmp.sources.luasnip";
-                enabled = true;
-              };
+              # buffer = {
+              #   name = "buffer";
+              #   module = "blink.cmp.sources.buffer";
+              #   enabled = true;
+              # };
+              # path = {
+              #   name = "path";
+              #   modules = "blink.cmp.sources.path";
+              #   enabled = true;
+              #   async = true;
+              # };
+              # luasnip = {
+              #   name = "luasnip";
+              #   module = "blink.cmp.sources.luasnip";
+              #   enabled = true;
+              # };
             };
           };
           keymap = {
