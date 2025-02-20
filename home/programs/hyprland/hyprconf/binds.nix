972453b3e036405fb53ff58e397cf0f99f792363
Immelancholy <lenalowes0@gmail.com>
2025-02-20 18:17:59 +0000

    playerctl

--- home/programs/hyprland/hyprconf/binds.nix
+++ home/programs/hyprland/hyprconf/binds.nix
@@ -47,8 +47,8 @@
     bindel = , XF86AudioRaiseVolume, exec, rmpc volume +1 # increase volume
 
     bindl  = , mouse:277, exec, $playerctl play-pause # toggle between media play and pause
+    bindl = $mod, P, exec, $playerctl play-pause
     bindel  = , mouse:281, exec, rmpc volume +1 # volume up
-
     bindel  = , mouse:282, exec, rmpc volume -1 # volume down
     bindl  = $mod, mouse:282, exec, $playerctl next #next
     bindl  = $mod, mouse:281, exec, $playerctl previous #previous,
