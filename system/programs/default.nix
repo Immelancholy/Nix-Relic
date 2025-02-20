6347901ca2b22de73745634daa0761adc65f2933
Immelancholy <lenalowes0@gmail.com>
2025-02-20 23:26:01 +0000

    hypr

--- system/programs/default.nix
+++ system/programs/default.nix
@@ -74,11 +74,6 @@
   services.mullvad-vpn.enable = true;
   services.mullvad-vpn.package = pkgs.mullvad-vpn;
 
-  programs.gamescope = {
-    enable = true;
-    capSysNice = true;
-  };
-
   programs.steam = {
     enable = true;
     gamescopeSession = {
