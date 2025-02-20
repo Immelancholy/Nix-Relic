6129ed8a3c98f15b38dfb10ca63eb63b9efed5f8
Immelancholy <lenalowes0@gmail.com>
2025-02-20 11:57:30 +0000

    window rule

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -54,7 +54,7 @@
             ];
             providers = {
               lsp = {
-                name = "LSP";
+                name = "lsp";
                 module = "blink.cmp.sources.lsp";
                 timeout_ms = 2000;
                 fallbacks = [];
