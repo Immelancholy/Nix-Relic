dac81e06e533e51717e912682fae4965ebb5128d
Immelancholy <lenalowes0@gmail.com>
2025-02-20 12:50:06 +0000

    blink-cmp

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -78,6 +78,25 @@
                 name = "Git";
                 module = "blink-cmp-git";
               };
+              ripgrep = {
+                async = true;
+                module = "blink-ripgrep";
+                name = "Ripgrep";
+                score_offset = 100;
+                opts = {
+                  prefix_min_len = 3;
+                  context_size = 5;
+                  max_filesize = "1M";
+                  project_root_marker = ".git";
+                  project_root_fallback = true;
+                  search_casing = "--ignore-case";
+                  additional_rg_options = {};
+                  fallback_to_regex_highlighting = true;
+                  ignore_paths = {};
+                  additional_paths = {};
+                  debug = false;
+                };
+              };
             };
           };
           keymap = {
@@ -112,6 +131,7 @@
           };
         };
       };
+      blink-ripgrep.enable = true; = true;
       blink-cmp-git = {
         enable = true;
         autoLoad = true;
