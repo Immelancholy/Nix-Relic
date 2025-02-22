79aab793aee38f1a1737111ce353c39cbaf1515d
Immelancholy <lenalowes0@gmail.com>
2025-02-22 15:26:40 +0000

    nvim

--- system/env.nix
+++ system/env.nix
@@ -14,4 +14,14 @@
       "${XDG_BIN_HOME}"
     ];
   };
+  environment.etc."xdg/user-dirs.defaults".text = ''
+    DESKTOP=Desktop
+    DOCUMENTS=Documents
+    DOWNLOAD=Downloads
+    MUSIC=Music
+    PICTURES=Pictures
+    PUBLICSHARE=Public
+    TEMPLATES=Templates
+    VIDEOS=Videos
+  '';
 }
