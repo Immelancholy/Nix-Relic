b54d0a918c02af6f10a7c13ba5dc33392bf5c8f4
Immelancholy <lenalowes0@gmail.com>
2025-02-22 02:12:18 +0000

    nvim

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -100,6 +100,13 @@
                 "dictionary"
                 "spell"
               ];
+              markdown = [
+                "buffer"
+                "path"
+                "snippets"
+                "dictionary"
+                "spell"
+              ];
             };
             providers = {
               lsp = {
