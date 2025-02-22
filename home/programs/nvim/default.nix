47ba604f6e8bf5e4bd2d48afc385e21fb39b8d31
Immelancholy <lenalowes0@gmail.com>
2025-02-22 03:07:59 +0000

    nvim

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -457,40 +457,6 @@
           ];
         };
       };
-      # cmp = {
-      #   autoLoad = true;
-      #   enable = true;
-      #   autoEnableSources = true;
-      #   settings = {
-      #     mapping = {
-      #       "<C-Space>" = "cmp.mapping.complete()";
-      #       "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-      #       "<C-e>" = "cmp.mapping.close()";
-      #       "<C-f>" = "cmp.mapping.scroll_docs(4)";
-      #       "<CR>" = "cmp.mapping.confirm({ select = true })";
-      #       "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-      #       "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-      #     };
-      #     sources = [
-      #       {
-      #         name = "nvim_lsp";
-      #       }
-      #       {
-      #         name = "buffer";
-      #       }
-      #       {
-      #         name = "async_path";
-      #       }
-      #       {
-      #         name = "treesitter";
-      #       }
-      #       {
-      #         name = "lspkind";
-      #       }
-      #     ];
-      #   };
-      # };
-      # cmp-nvim-lsp.enable = true;
       bufdelete.enable = true;
       treesitter = {
         autoLoad = true;
