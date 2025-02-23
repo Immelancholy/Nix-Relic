ae897daf8e008f3f097242c0fa27d7600351e5b3
Immelancholy <lenalowes0@gmail.com>
2025-02-23 00:27:56 +0000

    hyprexpo

---
+++ home/programs/hyprland/hyprconf/hyprexpo.nix
@@ -0,0 +1,12 @@
+{
+  wayland.windowManager.hyprland.extraConfig = ''
+    plugin {
+      hyprexpo {
+        columns = 3
+        gap_size = 5
+        bg_col = rgb(cba6f7)
+        workspace_method = center current
+      }
+    }
+  '';
+}
