10d9908892494b238fa0fab7a75f80547b77d924
Immelancholy <lenalowes0@gmail.com>
2025-02-23 01:06:58 +0000

    hyprexpo

--- home/programs/hyprland/hyprconf/hyprspace.nix
+++ home/programs/hyprland/hyprconf/hyprspace.nix
@@ -20,6 +20,8 @@
           panelHeight = 320
           drawActiveWorkspace = true
 
+          workspaceActiveBorder = rgba(cba6f7ff) rgba(89dcebff) rgba(f5e0dcff) 40deg
+
         }
       }
     '';
