4c278c6b966bd38d5a745560183b90164716c686
Immelancholy <lenalowes0@gmail.com>
2025-02-23 19:21:17 +0000

    rofitheme

--- home/programs/hyprland/rofi/default.nix
+++ home/programs/hyprland/rofi/default.nix
@@ -4,10 +4,6 @@
     package = pkgs.rofi-wayland-unwrapped;
     terminal = "kitty";
     font = "JetBrainsMono";
-    plugins = with pkgs; [
-      rofi-obsidian
-      rofi-mpd
-    ];
   };
   imports = [
     ./theme.nix
