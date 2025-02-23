b5d8a4f86985174ee634eebcb09f3551caf6de79
Immelancholy <lenalowes0@gmail.com>
2025-02-23 19:55:22 +0000

    rofitheme

--- home/programs/hyprland/rofi/default.nix
+++ home/programs/hyprland/rofi/default.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   programs.rofi = {
     enable = true;
-    font = "JetBrainsMono Nerd Font Mono";
+    font = "JetBrainsMono Nerd Font Mono 12";
     package = pkgs.rofi-wayland-unwrapped;
     terminal = "kitty";
   };
