776c9862d6a4ac72c484b34c42f9307c8fd3cae8
Immelancholy <lenalowes0@gmail.com>
2025-02-24 01:30:09 +0000

    yazi-plugins

--- home/programs/yazi.nix
+++ home/programs/yazi.nix
@@ -2,12 +2,21 @@
   inputs,
   pkgs,
   ...
-}: {
+}: let
+  plugins-repo = pkgs.fetchFromGitHub {
+    owner = "yazi-rs";
+    repo = "plugins";
+    rev = "5186af7984aa8cb0550358aefe751201d7a6b5a8";
+    hash = "";
+  };
+in {
   programs.yazi = {
     enable = true;
     package = inputs.yazi.packages.${pkgs.system}.default;
     enableZshIntegration = true;
-    plugins = {};
+    plugins = {
+      full-border = "${plugins-repo}/full-border.yazi";
+    };
     settings = {
       manager.show_hidden = true;
       opener = {
