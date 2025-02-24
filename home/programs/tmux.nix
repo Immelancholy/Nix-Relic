bf9a9825437c420b7220238888011272a3a38d62
Immelancholy <lenalowes0@gmail.com>
2025-02-24 20:49:49 +0000

    tmux

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -12,14 +12,7 @@
         extraConfig = ''
           set -g @catppuccin_flavor "mocha"
           set -g @catppuccin_window_status_style "rounded"
-          set -g status-right-length 100
-          set -g status-left-length 100
-          set -g status-left ""
-          set -g status-right "#{E:@catppuccin_status_application}"
-          set -agF status-right "#{E:@catppuccin_status_cpu}"
-          set -ag status-right "#{E:@catppuccin_status_session}"
-          set -ag status-right "#{E:@catppuccin_status_uptime}"
-          set -agF status-right "#{E:@catppuccin_status_battery}"
+
         '';
       }
       {
@@ -36,7 +29,14 @@
       bind-key l select-pane -R
       bind-key -r C-h select-window -t :-
       bind-key -r C-l select-window -t :+
-
+      set -g status-right-length 100
+      set -g status-left-length 100
+      set -g status-left ""
+      set -g status-right "#{E:@catppuccin_status_application}"
+      set -agF status-right "#{E:@catppuccin_status_cpu}"
+      set -ag status-right "#{E:@catppuccin_status_session}"
+      set -ag status-right "#{E:@catppuccin_status_uptime}"
+      set -agF status-right "#{E:@catppuccin_status_battery}"
 
     '';
   };
