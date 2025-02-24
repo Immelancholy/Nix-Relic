778faab625c285bd09ee28307ae7c0ba16f7a7ea
Immelancholy <lenalowes0@gmail.com>
2025-02-24 20:38:18 +0000

    tmux

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -22,6 +22,18 @@
       bind-key l select-pane -R
       bind-key -r C-h select-window -t :-
       bind-key -r C-l select-window -t :+
+
+      set -g status-right-length 100
+      set -g status-left-length 100
+      set -g status-left ""
+      set -g status-right "#{E:@catppuccin_status_application}"
+      set -agF status-right "#{E:@catppuccin_status_cpu}"
+      set -ag status-right "#{E:@catppuccin_status_session}"
+      set -ag status-right "#{E:@catppuccin_status_uptime}"
+      set -agF status-right "#{E:@catppuccin_status_battery}"
+
+      run ~/.config/tmux/plugins/tmux-plugins/tmux-cpu/cpu.tmux
+      run ~/.config/tmux/plugins/tmux-plugins/tmux-battery/battery.tmux
     '';
   };
 }
