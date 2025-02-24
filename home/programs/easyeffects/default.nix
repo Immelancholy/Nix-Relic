17219bebc3be5dace3ba8851e5abf5a516aba7be
Immelancholy <lenalowes0@gmail.com>
2025-02-24 12:09:25 +0000

    reorganise

--- home/programs/easyeffects/default.nix
+++ home/programs/easyeffects/default.nix
@@ -1,10 +1,9 @@
 {
+  imports = [
+    ./presets
+  ];
   services.easyeffects = {
     enable = true;
     preset = "Rode NT1";
-  };
-  xdg.configFile."easyeffects/input" = {
-    source = ./presets/input;
-    recursive = true;
   };
 }
