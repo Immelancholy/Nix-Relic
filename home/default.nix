74947146780d59a0385f86313e7a5ff92e936fef
Immelancholy <lenalowes0@gmail.com>
2025-02-24 11:59:23 +0000

    reorganise

--- home/default.nix
+++ home/default.nix
@@ -4,7 +4,6 @@
   lib,
   ...
 }: {
-  # TODO please change the username & home directory to your own
   home.username = "mela";
   home.homeDirectory = "/home/mela";
 
@@ -31,53 +30,17 @@
     ./mimeapps.nix
   ];
 
-  # Packages that should be installed to the user profile.
-  xdg.enable = true;
-
-  programs.mpv = {
-    enable = true;
-
-    package = (
-      pkgs.mpv-unwrapped.wrapper {
-        scripts = with pkgs.mpvScripts; [
-          uosc
-          sponsorblock
-        ];
-
-        mpv = pkgs.mpv-unwrapped.override {
-          waylandSupport = true;
-        };
-      }
-    );
-
-    config = {
-      profile = "high-quality";
-      ytdl-format = "bestvideo+bestaudio";
-      cache-default = 4000000;
-    };
-  };
-
   home.file = {
-    ".local/share/bin" = {
-      source = ./programs/scripts;
+    "Pictures/fastfetch_logos" = {
+      source = ./programs/fastfetch/logo;
       recursive = true;
     };
-  };
-  home.file = {
-    ".zen/yzyavqg4.default" = {
-      source = ./programs/zen;
-      recursive = true;
-    };
-  };
-  home.file = {
     "Pictures/wallpapers" = {
       source = ./backgrounds;
       recursive = true;
     };
-  };
-  home.file = {
-    "Pictures/fastfetch_logos" = {
-      source = ./programs/fastfetch/logo;
+    ".zen/yzyavqg4.default" = {
+      source = ./programs/zen;
       recursive = true;
     };
   };
@@ -85,8 +48,13 @@
   fonts.fontconfig = {
     enable = true;
   };
-  xdg.configFile."autostart/input-mapper-autoload.desktop" = lib.mkIf nixosConfig.services.input-remapper.enable {
-    source = "${nixosConfig.services.input-remapper.package}/share/applications/input-remapper-autoload.desktop";
+  xdg = {
+    enable = true;
+    configFile = {
+      "autostart/input-mapper-autoload.desktop" = lib.mkIf nixosConfig.services.input-remapper.enable {
+        source = "${nixosConfig.services.input-remapper.package}/share/applications/input-remapper-autoload.desktop";
+      };
+    };
   };
 
   home.stateVersion = "24.11";
--- home/programs/default.nix
+++ home/programs/default.nix
@@ -13,7 +13,30 @@
     ./nvim
     ./easyeffects
     ./rmpc
+    ./scripts
   ];
+  programs.mpv = {
+    enable = true;
+
+    package = (
+      pkgs.mpv-unwrapped.wrapper {
+        scripts = with pkgs.mpvScripts; [
+          uosc
+          sponsorblock
+        ];
+
+        mpv = pkgs.mpv-unwrapped.override {
+          waylandSupport = true;
+        };
+      }
+    );
+
+    config = {
+      profile = "high-quality";
+      ytdl-format = "bestvideo+bestaudio";
+      cache-default = 4000000;
+    };
+  };
 
   programs.kitty = {
     enable = true;
