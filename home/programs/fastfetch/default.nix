73d35974706d3dce00fc2547c590099d27e91872
Immelancholy <lenalowes0@gmail.com>
2025-02-24 14:52:34 +0000

    tmux

--- home/programs/fastfetch/default.nix
+++ home/programs/fastfetch/default.nix
@@ -6,7 +6,7 @@
         type = "kitty-icat";
         source = "$HOME/Pictures/fastfetch_logos/tomoe.png";
         height = 18;
-        width = 29;
+        width = 28;
       };
       display = {
         separator = " : ";
