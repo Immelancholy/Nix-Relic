5d5dc18566d8339026432e5bee462f936bcb2593
Immelancholy <lenalowes0@gmail.com>
2025-02-24 21:33:51 +0000

    tmux

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -9,23 +9,12 @@
     plugins = with pkgs; [
       tmuxPlugins.sensible
       tmuxPlugins.pain-control
-      tmuxPlugins.battery
-      tmuxPlugins.cpu
-      # {
-      #   plugin = tmuxPlugins.catppuccin;
-      #   extraConfig = ''
-      #     set -g @catppuccin_flavor "mocha"
-      #     set -g @catppuccin_window_status_style "rounded"
-      #     set -g status-right-length 100
-      #     set -g status-left-length 100
-      #     set -g status-left ""
-      #     set -g status-right "#{E:@catppuccin_status_application}"
-      #     set -agF status-right "#{E:@catppuccin_status_cpu}"
-      #     set -ag status-right "#{E:@catppuccin_status_session}"
-      #     set -ag status-right "#{E:@catppuccin_status_uptime}"
-      #     set -agF status-right "#{E:@catppuccin_status_battery}"
-      #   '';
-      # }
+      {
+        plugin = tmuxPlugins.cpu;
+        extraConfig = ''
+          set -agF status-right "#{E:@catppuccin_status_cpu}"
+        '';
+      }
     ];
   };
 }
