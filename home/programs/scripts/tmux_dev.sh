276d131ad58150c4f6b499f43d9ceace38e61347
Immelancholy <lenalowes0@gmail.com>
2025-02-25 15:07:57 +0000

    dev sesh

--- home/programs/scripts/tmux_dev.sh
+++ home/programs/scripts/tmux_dev.sh
@@ -7,9 +7,9 @@ sesh="dev$i"
 while tmux has-session -t $sesh 2>/dev/null; do
 	((i++))
 done
-tmux new -d -s $sesh 'nv.sh; $SHELL' 
+tmux new -d -s $sesh 
 
-tmux new-window 'lazygit; $SHELL'
+tmux new-window 
 
 tmux previous-window
 
