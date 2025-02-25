df9169ffccc1277703f2c12f77b2ee9a292757c3
Immelancholy <lenalowes0@gmail.com>
2025-02-25 22:54:06 +0000

    wez

--- home/programs/fastfetch/default.nix
+++ home/programs/fastfetch/default.nix
@@ -5,7 +5,7 @@
       logo = {
         type = "kitty-icat";
         source = "$HOME/Pictures/fastfetch_logos/tomoe.png";
-        width = 28;
+        width = 25;
         # height = 18;
       };
       display = {
