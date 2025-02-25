e43ee4c930abb97a8f026e9c6459a50e8aee7917
Immelancholy <lenalowes0@gmail.com>
2025-02-25 12:35:26 +0000

    screenshot

--- home/programs/scripts/screenshot.sh
+++ home/programs/scripts/screenshot.sh
@@ -35,7 +35,7 @@ if [ -z "$XDG_SCREENSHOTS_DIR" ]; then
 	XDG_SCREENSHOTS_DIR="$HOME/Pictures/Screenshots"
 fi
 
-confDir="${XDG_CONFIG_HOME}"
+confDir="${confDir:-XDG_CONFIG_HOME}"
 save_dir="${XDG_SCREENSHOTS_DIR}"
 mkdir -p "$save_dir"
 save_file=$(date +'%y%m%d_%Hh%Mm%Ss_screenshot.png')
@@ -44,7 +44,7 @@ annotation_tool=${GRIMBLAST_EDITOR}
 annotation_args=("-o" "${save_dir}/${save_file}" "-f" "${temp_screenshot}")
 
 
-swpy_dir="${configDir}/swappy"
+swpy_dir="${confDir}/swappy"
 mkdir -p "$swpy_dir"
 echo -e "[Default]\nsave_dir=$save_dir\nsave_filename_format=$save_file" >"${swpy_dir}"/config
 
