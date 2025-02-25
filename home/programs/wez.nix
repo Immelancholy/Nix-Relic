19c832c8ab8470e2f2ce6e3d47200397aa028bab
Immelancholy <lenalowes0@gmail.com>
2025-02-25 21:05:49 +0000

    wez?

--- home/programs/wez.nix
+++ home/programs/wez.nix
@@ -16,7 +16,7 @@
 
       config.font = wezterm.font ({
         family = 'CaskaydiaCove Nerd Font',
-        stretch = 'Regular',
+        stretch = 'Normal',
         weight = 'Regular',
       })
 
