04ea7d9598187515bac4dd6bc2b231e2bb662cb7
Immelancholy <lenalowes0@gmail.com>
2025-02-25 23:17:15 +0000

    wez

--- home/programs/scripts/cava.sh
+++ home/programs/scripts/cava.sh
@@ -2,7 +2,7 @@
 
 # Toggle kitty padding when starting/quitting a vim instance
 kitty @ set-spacing padding=0
-kitty @ set-font-size 9
+kitty @ set-font-size 3
 cava "$@"
 kitty @ set-spacing padding=default
 kitty @ set-font-size default
