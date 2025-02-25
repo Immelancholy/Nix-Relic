08499d6a16ac0943e8a9ce5c3e95d1006ad1ad60
Immelancholy <lenalowes0@gmail.com>
2025-02-25 14:33:17 +0000

    dev sesh

--- home/programs/scripts/tmux_dev.sh
+++ home/programs/scripts/tmux_dev.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 i=0
-sesh=${"dev+$i"}
+sesh=${"dev$i"}
 
 # Check if the session already exists
 while tmux has-session -t $sesh 2>/dev/null; do
 	((i++))
 done
-tmux new -s $sesh -d 'nv.sh; $SHELL'
-tmux split-window -h
-tmux neww 'lazygit; $SHELL'
-tmux attach-session -t $sesh
+tmux new-session -d -s $sesh
 
+tmux split-window -h
+
+tmux send-keys -t 0 'nv.sh' C-m
 
 
