2d9349626a31f6b575e59bf10bb5f38b465f49f3
Immelancholy <lenalowes0@gmail.com>
2025-02-26 01:22:31 +0000

    cava

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -8,14 +8,14 @@
       bold_italic_font = "auto";
       enable_audio_bell = true;
       font_size = 10;
-      window_padding_width = 10;
+      window_padding_width = 5;
       allow_remote_control = true;
       listen_on = "unix:/run/user/1000/mykitty";
     };
     extraConfig = ''
       background_opacity 0.8
       confirm_os_window_close 0
-      # placement_strategy top
+      placement_strategy center
       cursor_trail 10
       cursor_trail_start_threshold 0
       shell_integration no-cursor
