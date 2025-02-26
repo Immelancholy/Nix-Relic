c7b4dd56184ef0f783e773138a4f49af5309ddc0
Immelancholy <lenalowes0@gmail.com>
2025-02-26 00:18:20 +0000

    rmpc

--- home/programs/scripts/rmpc.sh
+++ home/programs/scripts/rmpc.sh
@@ -3,6 +3,8 @@
 # Toggle kitty padding when starting/quitting a vim instance
 kitty @ set-spacing padding=2
 kitty @ set-font-size 9
+kitty @ set-font "JetBrainsMono Nerd Font Mono"
 rmpc "$@"
 kitty @ set-spacing padding=default
 kitty @ set-font-size 10 
+kitty @ set-font default
