42b7114e4df1d2c83456b1aa797dc05f349b0813
Immelancholy <lenalowes0@gmail.com>
2025-02-26 21:56:16 +0000

    weee

--- home/obs.nix
+++ home/obs.nix
@@ -1,6 +1,14 @@
 {pkgs, ...}: {
   programs.obs-studio = {
     enable = true;
-    package = null;
+    package = pkgs.obs-studio.override {
+      enableVirtualCamera = true;
+    };
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-vkcapture
+      obs-vaapi
+      obs-pipewire-audio-capture
+      obs-composite-blur
+    ];
   };
 }
--- system/default.nix
+++ system/default.nix
@@ -19,17 +19,6 @@
     priority = 100;
   };
 
-  programs.obs-studio = {
-    enable = true;
-    enableVirtualCamera = true;
-    plugins = with pkgs.obs-studio-plugins; [
-      obs-vkcapture
-      obs-vaapi
-      obs-pipewire-audio-capture
-      obs-composite-blur
-    ];
-  };
-
   security.apparmor = {
     enable = true;
     packages = with pkgs; [
