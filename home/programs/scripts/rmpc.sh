cc390b81c0b728759cd6a9604e49b9349ab0bc52
Immelancholy <lenalowes0@gmail.com>
2025-02-26 10:44:39 +0000

    fantasque

--- home/programs/scripts/rmpc.sh
+++ home/programs/scripts/rmpc.sh
@@ -2,7 +2,7 @@
 
 # Toggle kitty padding when starting/quitting a vim instance
 kitty @ set-spacing padding=2
-kitty @ set-font-size 9
+kitty @ set-font-size 9.5
 rmpc "$@"
 kitty @ set-spacing padding=default
 kitty @ set-font-size 10 
--- system/fonts/default.nix
+++ system/fonts/default.nix
@@ -22,7 +22,7 @@
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
-      monospace = ["JetBrainsMono Nerd Font Mono"];
+      monospace = ["CaskaydiaCove Nerd Font Mono"];
       serif = ["Noto Serif Nerd Font"];
       sansSerif = ["Noto Sans Nerd Font"];
       emoji = ["Noto Color Emoji" "Symbols Only Nerd Font"];
