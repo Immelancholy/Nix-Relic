def839d3eae05c346c7d5902464e1d568392deaf
Immelancholy <lenalowes0@gmail.com>
2025-02-26 12:04:15 +0000

    stylix

--- home/themeing.nix
+++ home/themeing.nix
@@ -1,46 +1,46 @@
 {pkgs, ...}: {
-  catppuccin = {
-    enable = true;
-    flavor = "mocha";
-    accent = "mauve";
-    yazi = {
-      enable = true;
-      accent = "mauve";
-      flavor = "mocha";
-    };
-    # gtk = {
-    #   enable = true;
-    #   accent = "mauve";
-    #   flavor = "mocha";
-    # };
-    # btop = {
-    #   enable = true;
-    #   flavor = "mocha";
-    # };
-    rofi = {
-      enable = false;
-      flavor = "mocha";
-    };
-    waybar.enable = false;
-    dunst = {
-      enable = false;
-    };
-    tmux = {
-      enable = true;
-      flavor = "mocha";
-      extraConfig = ''
-
-        set -g @catppuccin_window_status_style "rounded"
-        set -g status-right-length 100
-        set -g status-left-length 100
-        set -g status-left ""
-        set -g status-right "#{E:@catppuccin_status_application}"
-        set -ag status-right "#{E:@catppuccin_status_session}"
-        set -ag status-right "#{E:@catppuccin_status_uptime}"
-      '';
-    };
-    kvantum.enable = false;
-  };
+  # catppuccin = {
+  #   enable = true;
+  #   flavor = "mocha";
+  #   accent = "mauve";
+  #   yazi = {
+  #     enable = true;
+  #     accent = "mauve";
+  #     flavor = "mocha";
+  #   };
+  #   # gtk = {
+  #   #   enable = true;
+  #   #   accent = "mauve";
+  #   #   flavor = "mocha";
+  #   # };
+  #   # btop = {
+  #   #   enable = true;
+  #   #   flavor = "mocha";
+  #   # };
+  #   rofi = {
+  #     enable = false;
+  #     flavor = "mocha";
+  #   };
+  #   waybar.enable = false;
+  #   dunst = {
+  #     enable = false;
+  #   };
+  #   tmux = {
+  #     enable = true;
+  #     flavor = "mocha";
+  #     extraConfig = ''
+  #
+  #       set -g @catppuccin_window_status_style "rounded"
+  #       set -g status-right-length 100
+  #       set -g status-left-length 100
+  #       set -g status-left ""
+  #       set -g status-right "#{E:@catppuccin_status_application}"
+  #       set -ag status-right "#{E:@catppuccin_status_session}"
+  #       set -ag status-right "#{E:@catppuccin_status_uptime}"
+  #     '';
+  #   };
+  #   kvantum.enable = false;
+  # };
 
   gtk = {
     enable = true;
