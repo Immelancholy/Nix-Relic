ecd3512dae5b56d7ad4006899e407d4a8c84438d
Immelancholy <lenalowes0@gmail.com>
2025-02-26 10:42:19 +0000

    fantasque

--- system/fonts/default.nix
+++ system/fonts/default.nix
@@ -22,7 +22,7 @@
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
-      monospace = ["FantasqeSansM Nerd Font Mono"];
+      monospace = ["JetBrainsMono Nerd Font Mono"];
       serif = ["Noto Serif Nerd Font"];
       sansSerif = ["Noto Sans Nerd Font"];
       emoji = ["Noto Color Emoji" "Symbols Only Nerd Font"];
