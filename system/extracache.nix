daa66c78967db14dd53dd1ae597609732fc2fe12
Immelancholy <lenalowes0@gmail.com>
2025-02-27 17:33:46 +0000

    switch

--- system/extracache.nix
+++ system/extracache.nix
@@ -6,14 +6,12 @@
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
       "https://prismlauncher.cachix.org"
-      "https://wezterm.cachix.org"
     ];
 
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "prismlauncher.cachix.org-1:9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c="
-      "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
     ];
   };
 }
