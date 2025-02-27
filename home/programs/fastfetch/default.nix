10f94352847dd0a599503440f5f4d86f0a8105f8
Immelancholy <lenalowes0@gmail.com>
2025-02-27 12:19:44 +0000

    fastfetch

--- home/programs/fastfetch/default.nix
+++ home/programs/fastfetch/default.nix
@@ -54,7 +54,7 @@
           type = "command";
           key = "   Shell";
           KeyColor = "yellow";
-          text = ''shell=$($SHELL --version);echo $shell'';
+          text = ''shell=$($SHELL --version | cut -d ' ' -f 1);echo $shell'';
         }
         {
           type = "WM";
