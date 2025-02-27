3e90dc9be547c51bd85176e2ab11156bffbde885
Immelancholy <lenalowes0@gmail.com>
2025-02-27 21:37:00 +0000

    switch

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -18,27 +18,27 @@
     ./tmux.nix
     ./kitty.nix
   ];
-  programs.mpv = {
-    enable = true;
-
-    package = (
-      pkgs.mpv-unwrapped.wrapper {
-        scripts = with pkgs.mpvScripts; [
-          uosc
-          sponsorblock
-        ];
-        mpv = pkgs.mpv-unwrapped.override {
-          waylandSupport = true;
-        };
-      }
-    );
-
-    config = {
-      profile = "high-quality";
-      ytdl-format = "bestvideo+bestaudio";
-      cache-default = 4000000;
-    };
-  };
+  # programs.mpv = {
+  #   enable = true;
+  #
+  #   package = (
+  #     pkgs.mpv-unwrapped.wrapper {
+  #       scripts = with pkgs.mpvScripts; [
+  #         uosc
+  #         sponsorblock
+  #       ];
+  #       mpv = pkgs.mpv-unwrapped.override {
+  #         waylandSupport = true;
+  #       };
+  #     }
+  #   );
+  #
+  #   config = {
+  #     profile = "high-quality";
+  #     ytdl-format = "bestvideo+bestaudio";
+  #     cache-default = 4000000;
+  #   };
+  # };
 
   programs.btop = {
     enable = true;
