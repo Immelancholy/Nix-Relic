4d0fad03afe298b71b5f9adeb7adef9f6e8057ae
Immelancholy <lenalowes0@gmail.com>
2025-02-27 23:59:52 +0000

    switch

--- system/default.nix
+++ system/default.nix
@@ -14,6 +14,7 @@
     ./env.nix
   ];
   services.gnome.gnome-keyring.enable = true;
+  systemd.user.sessionVariables.SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
 
   nixpkgs.config.allowUnfree = true;
 
