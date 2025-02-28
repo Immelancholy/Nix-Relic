80ce91158d08627f59ffd2460612d8f055f4bfa8
Immelancholy <lenalowes0@gmail.com>
2025-02-28 03:37:03 +0000

    Gnome Keyring

--- home/zsh.nix
+++ home/zsh.nix
@@ -45,9 +45,6 @@
     profileExtra = ''
       eval $(gnome-keyring-daemon --start --daemonize)
     '';
-    # loginExtra = ''
-    #   eval $(gnome-keyring-daemon --start --daemonize)
-    # '';
     initExtra = ''
       source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
       source <(fzf --zsh)
