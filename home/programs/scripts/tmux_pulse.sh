0f528c11fb7ee5271862f98a2ce8dc59feaf0003
Immelancholy <lenalowes0@gmail.com>
2025-02-28 15:02:30 +0000

    switch

---
+++ home/programs/scripts/tmux_pulse.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+i=0
+sesh="pulsemixer"
+
+# Check if the session already exists
+while tmux has-session -t $sesh 2>/dev/null; do
+	((i++))
+	sesh="dev$i"
+done
+tmux new -d -s $sesh "pulsemixer; $SHELL" 
+
+tmux split-window -v
+
+tmux select-pane -t 0
+
+tmux resize-pane -D 80
+
+tmux attach-session -t $sesh
+
