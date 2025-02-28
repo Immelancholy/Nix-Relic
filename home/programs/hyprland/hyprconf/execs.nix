275ce43d7c18905b13a1f3292919ef45b7724054
Immelancholy <lenalowes0@gmail.com>
2025-02-28 00:21:06 +0000

    switch

--- home/programs/hyprland/hyprconf/execs.nix
+++ home/programs/hyprland/hyprconf/execs.nix
@@ -3,6 +3,8 @@
     exec-once = [
       "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
       "hyprctl setcursor Bibata-Modern-Ice 20"
+      "gnome-keyring-daemon --start --components=pkcs11,secrets,ssh"
+      "export SSH_AUTH_SOCK"
       "$scr/swww.sh"
       "wl-paste --type text --watch cliphist store # Stores only image data"
       "wl-paste --type image --watch cliphist store # Stores only image data"
