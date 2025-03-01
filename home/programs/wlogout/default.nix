2ee68afc5bf5885bb86a3cbe90af9e864b42b7d2
Immelancholy <lenalowes0@gmail.com>
2025-03-01 01:38:32 +0000

    switch

--- home/programs/wlogout/default.nix
+++ home/programs/wlogout/default.nix
@@ -1,5 +1,43 @@
 {
   programs.wlogout = {
-    enable =true;
+    enable = true;
+    layout = [
+      {
+        label = "lock";
+        action = "swaylock; loginctl suspend";
+        text = "Lock";
+        keybind = "l";
+      }
+      {
+        label = "hibernate";
+        action = "systemctl hibernate";
+        text = "Hibernate";
+        keybind = "h";
+      }
+      {
+        label = "logout";
+        action = "pkill -u $USER";
+        text = "Logout";
+        keybind = "e";
+      }
+      {
+        label = "shutdown";
+        action = "systemctl poweroff";
+        text = "Shutdown";
+        keybind = "s";
+      }
+      {
+        label = "suspend";
+        action = "systemctl suspend";
+        text = "Suspend";
+        keybind = "u";
+      }
+      {
+        label = "reboot";
+        action = "systemctl reboot";
+        text = "Reboot";
+        keybind = "r";
+      }
+    ];
   };
 }
