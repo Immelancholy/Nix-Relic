3cd593d7cd5daaa21906263705e3106d5defced7
Immelancholy <lenalowes0@gmail.com>
2025-03-03 03:15:25 +0000

    switch

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -423,6 +423,14 @@
               }
             ];
           }
+          {
+            __unkeyed-1 = "yuck-vim";
+            after = ''
+              function()
+                require("yuck").setup()
+              end
+            '';
+          }
         ];
       };
       snacks = {
