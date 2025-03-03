159673ff1b089898a30c9615155c3c9823672a2f
Immelancholy <lenalowes0@gmail.com>
2025-03-03 10:59:45 +0000

    switch

--- home/programs/scripts/tmux_nix.sh
+++ home/programs/scripts/tmux_nix.sh
@@ -8,9 +8,7 @@ while tmux has-session -t $sesh 2>/dev/null; do
 	((i++))
 	sesh="NixDots$i"
 done
-tmux new -d -s $sesh -c /etc/nixos
-
-tmux send-keys 'yazi' C-m
+tmux new -d -s $sesh -c /etc/nixos "nv.sh; $SHELL"
 
 tmux new-window -c /etc/nixos "lazygit; $SHELL" 
 
