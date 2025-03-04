f4527a64e9ee245b717ab594e1d362c6984722fb
Immelancholy <lenalowes0@gmail.com>
2025-03-04 16:42:17 +0000

    switch

--- home/programs/swayidle.nix
+++ home/programs/swayidle.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   services.swayidle = {
     enable = true;
     events = [
@@ -9,13 +13,15 @@
     ];
     timeouts = [
       {
-        timeout = 900;
+        # timeout = 900;
+        timeout = 30;
         command = "${pkgs.swaylock}/bin/swaylock -fF";
       }
       {
-        timeout = 905;
-        command = "hyprctl dispatch dpms off";
-        resumeCommand = "hyprctl dispatch dpms on";
+        # timeout = 905;
+        timeout = 35;
+        command = "${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl dispatch dpms off";
+        resumeCommand = "${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl dispatch dpms on";
       }
     ];
   };
