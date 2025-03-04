957328b3a16b483e0ff7270662324b9edd0dd2ee
Immelancholy <lenalowes0@gmail.com>
2025-03-04 20:55:50 +0000

    switch

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -13,7 +13,8 @@
       listen_on = "unix:/run/user/1000/mykitty";
     };
     extraConfig = ''
-      background_opacity 0.67
+      # background_opacity 0.67
+      background_opacity 0.80
       confirm_os_window_close 0
       placement_strategy center
       resize_in_steps yes
