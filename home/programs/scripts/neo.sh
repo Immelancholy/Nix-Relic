f3f1d3a3791ce214d71116a0ce16e0ec3de080a9
Immelancholy <lenalowes0@gmail.com>
2025-03-09 14:25:59 +0000

    switch

--- home/programs/scripts/neo.sh
+++ home/programs/scripts/neo.sh
@@ -2,5 +2,5 @@
 
 # Toggle kitty padding when starting/quitting a vim instance
 kitty @ set-spacing padding=0
-neo -a -S 20 -d 1 -f 144 -D -C /home/mela/.local/share/bin/colors -b 1 -m "Welcome, ${USER^}." --lingerms=1,1 --rippct=0 "$@"
+neo -a -S 20 -d 1 -f 144 -D -C "{$HOME}"/.local/share/bin/colors -b 1 -m "Welcome, ${USER^}." --lingerms=1,1 --rippct=0 "$@"
 kitty @ set-spacing padding=default
