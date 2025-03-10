2a7f3a3296f2894e1907280ec61e0259871d2314
Immelancholy <lenalowes0@gmail.com>
2025-03-10 15:37:24 +0000

    switch

--- home/themeing.nix
+++ home/themeing.nix
@@ -64,9 +64,9 @@
   };
 
   home.pointerCursor = {
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Ice";
-    size = 20;
+    # package = pkgs.bibata-cursors;
+    # name = "Bibata-Modern-Ice";
+    # size = 20;
     gtk.enable = true;
     x11.enable = true;
     hyprcursor = {
