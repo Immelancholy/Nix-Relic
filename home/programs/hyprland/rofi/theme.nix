f0d017353f5669e19acdd770ef92b8cde3d62bb8
Immelancholy <lenalowes0@gmail.com>
2025-03-18 15:29:51 +0000

    switch

--- home/programs/hyprland/rofi/theme.nix
+++ home/programs/hyprland/rofi/theme.nix
@@ -122,7 +122,7 @@
         text-color = mkLiteral "@text";
       };
       "button" = {
-        width = mkLiteral "85px";
+        width = mkLiteral "90px";
         padding = mkLiteral "0px";
         border-radius = mkLiteral "100%";
         background-color = mkLiteral "@crust";
