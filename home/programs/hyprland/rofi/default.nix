17c10622739820013ad0ae730d349f70b8e1a1fa
Immelancholy <lenalowes0@gmail.com>
2025-03-18 12:59:14 +0000

    switch

--- home/programs/hyprland/rofi/default.nix
+++ home/programs/hyprland/rofi/default.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   programs.rofi = {
     enable = true;
-    font = "JetBrainsMono Nerd Font Mono 10";
+    font = "JetBrainsMono Nerd Font Mono 12";
     package = pkgs.rofi-wayland;
     terminal = "kitty";
     extraConfig = {
