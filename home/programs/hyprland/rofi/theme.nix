4df8448eaaee7c837d7f21b14365f1d5f2ed0b32
Immelancholy <lenalowes0@gmail.com>
2025-03-18 13:07:20 +0000

    switch

--- home/programs/hyprland/rofi/theme.nix
+++ home/programs/hyprland/rofi/theme.nix
@@ -37,9 +37,9 @@
         surface2 = mkLiteral "#585b70";
         surface1 = mkLiteral "#45475a";
         surface0 = mkLiteral "#313244";
-        base = mkLiteral "#1e1e2eCD";
-        mantle = mkLiteral "#181825CD";
-        crust = mkLiteral "#11111bCD";
+        base = mkLiteral "#1e1e2eAB";
+        mantle = mkLiteral "#181825AB";
+        crust = mkLiteral "#11111bAB";
         wallpaper = mkLiteral ''url("${../../../backgrounds/Neon-Beast-Girl.png}", width)'';
 
         lines = mkLiteral "6";
@@ -58,7 +58,7 @@
         enabled = mkLiteral "true";
         border = mkLiteral "2px solid";
         border-radius = mkLiteral "15px";
-        # border-color = mkLiteral "@mauve";
+        border-color = mkLiteral "@crust";
         cursor = "default";
         background-color = mkLiteral "@base";
       };
