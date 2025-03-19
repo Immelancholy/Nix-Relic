16c6c85c64486b174346333f7ba0a8afb764da0d
Immelancholy <lenalowes0@gmail.com>
2025-03-19 20:29:01 +0000

    switch

--- system/programs/flatpak.nix
+++ system/programs/flatpak.nix
@@ -13,6 +13,7 @@
       "com.obsproject.Studio.Plugin.OBSVkCapture"
       "org.freedesktop.Platform.VulkanLayer.OBSVkCapture/x86_64/24.08"
       "com.obsproject.Studio.Plugin.GStreamerVaapi"
+      "org.freedesktop.Platform.GStreamer.gstreamer-vaapi//22.08"
     ];
   };
 }
