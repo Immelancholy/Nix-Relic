0477b73b57bb1e259498dcd0e9142b6493e4fec8
Immelancholy <lenalowes0@gmail.com>
2025-03-19 21:22:22 +0000

    switch

--- home/programs/obs.nix
+++ home/programs/obs.nix
@@ -2,10 +2,9 @@
   programs.obs-studio = {
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
-      wlrobs
-      obs-vkcapture
-      droidcam-obs
-      obs-pipewire-audio-capture
+      # obs-vkcapture
+      # droidcam-obs
+      # obs-pipewire-audio-capture
     ];
   };
 }
