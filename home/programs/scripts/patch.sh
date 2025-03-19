9ac333fb9a4dd8bf5c4f95e1201259bbdde00c59
Immelancholy <lenalowes0@gmail.com>
2025-03-19 15:12:12 +0000

    switch

--- home/programs/scripts/patch.sh
+++ home/programs/scripts/patch.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash 
 pkill qpwgraph
 cd /home/mela/.config/rncbc.org/ || exit
 mv qpwgraph.conf qpwgraph_notuse1.conf
 mv qpwgraph_notuse.conf qpwgraph.conf
 mv qpwgraph_notuse1.conf qpwgraph_notuse.conf
-exec qpwgraph
+qpwgraph
+qpwgraph
