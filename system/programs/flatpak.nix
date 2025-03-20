45d48b2ce59a96022a6cfc24befee2a4c8368536
Immelancholy <lenalowes0@gmail.com>
2025-03-20 22:03:12 +0000

    switch

--- system/programs/flatpak.nix
+++ system/programs/flatpak.nix
@@ -2,8 +2,6 @@
   services.flatpak = {
     overrides = {
       global = {
-        Context.sockets = ["wayland" "!x11" "!fallback-x11"];
-
         Environment = {
           XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
         };
