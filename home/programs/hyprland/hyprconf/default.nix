f04c7faf537fc2e70b19f78182bbcb659497436b
Immelancholy <lenalowes0@gmail.com>
2025-03-23 18:47:20 +0000

    monitor

--- home/programs/hyprland/hyprconf/default.nix
+++ home/programs/hyprland/hyprconf/default.nix
@@ -11,8 +11,8 @@
     "$edit" = "$term $scr/nv.sh";
     "$player" = "mpd";
     "$playerctl" = "uwsm app -- playerctl --player=$player";
-    # monitor = ", preferred, auto, 1";
-    monitor = "HDMI-A-1, 1920x1080@144, 0x0, 1";
+    monitor = ", preferred, auto, 1";
+    # monitor = "HDMI-A-1, 1920x1080@144, 0x0, 1";
     input = {
       kb_layout = "gb";
       kb_options = "fkeys:basic_13-24";
