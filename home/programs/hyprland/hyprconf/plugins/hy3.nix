e9decc59eafb2544c79329689de4a7419e5837d3
Immelancholy <lenalowes0@gmail.com>
2025-03-24 16:09:45 +0000

    switch

--- home/programs/hyprland/hyprconf/plugins/hy3.nix
+++ home/programs/hyprland/hyprconf/plugins/hy3.nix
@@ -10,6 +10,15 @@
     extraConfig = ''
       plugin {
         hy3 {
+          tabs {
+            border_width = 2
+            col.active = rgba(1e1e2eCC)
+            col.active.border = rgba(cba6f7ff)
+            col.inactive = rgba(1e1e2eCC)
+            col.inactive.border = rgba(b4befeff)
+            col.urgent = rgba (1e1e2eCC)
+            col.urgent.border = rgba(f9e2afff)
+          }
           # no_gaps_when_only = 1
           group_inset = 4
           autotile {
