aab545aae45e98e3ddd662b05f522e6ac1264eca
Immelancholy <lenalowes0@gmail.com>
2025-03-24 16:19:23 +0000

    switch

--- home/programs/hyprland/hyprconf/plugins/hy3.nix
+++ home/programs/hyprland/hyprconf/plugins/hy3.nix
@@ -12,11 +12,11 @@
         hy3 {
           tabs {
             border_width = 2
-            col.active = rgba(1e1e2eB9)
+            col.active = rgba(1e1e2eB5)
             col.active.border = rgba(cba6f7ff)
-            col.inactive = rgba(1e1e2eB9)
+            col.inactive = rgba(1e1e2eB5)
             col.inactive.border = rgba(b4befeff)
-            col.urgent = rgba(1e1e2eB9)
+            col.urgent = rgba(1e1e2eB5)
             col.urgent.border = rgba(f9e2afff)
           }
           # no_gaps_when_only = 1
