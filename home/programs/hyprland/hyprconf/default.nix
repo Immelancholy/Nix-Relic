dc382b0bc4e9589340f85453434e2b33483987d9
Immelancholy <lenalowes0@gmail.com>
2025-03-24 05:00:04 +0000

    switch

--- home/programs/hyprland/hyprconf/default.nix
+++ home/programs/hyprland/hyprconf/default.nix
@@ -24,7 +24,7 @@
     };
     misc = {
       disable_hyprland_logo = true;
-      disavle_splash_rendering = true;
+      disable_splash_rendering = true;
       key_press_enables_dpms = true;
     };
     # dwindle = {
