8caab6d509dc2b2e9b45d79ab9fc66144e010335
Immelancholy <lenalowes0@gmail.com>
2025-03-25 17:39:28 +0000

    switch

--- system/programs/hyprland.nix
+++ system/programs/hyprland.nix
@@ -18,8 +18,8 @@
       enable = true;
       package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
       portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-      xwayland.enable = true;
-      withUWSM = true;
+      # xwayland.enable = true;
+      # withUWSM = true;
     };
   };
 }
