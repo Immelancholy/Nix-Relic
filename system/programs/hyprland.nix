e3d5a7bde38792d6bac42a6bb5d16fadc2185c56
Immelancholy <lenalowes0@gmail.com>
2025-03-25 17:54:19 +0000

    switch

--- system/programs/hyprland.nix
+++ system/programs/hyprland.nix
@@ -6,20 +6,19 @@
   programs = {
     uwsm = {
       enable = true;
-      waylandCompositors = {
-        hyprland = {
-          prettyName = "Hyprland";
-          comment = "Hyprland compositor managed by UWSM";
-          binPath = "/run/current-system/sw/bin/Hyprland";
-        };
-      };
+      # waylandCompositors = {
+      #   hyprland = {
+      #     prettyName = "Hyprland";
+      #     comment = "Hyprland compositor managed by UWSM";
+      #     binPath = "/run/current-system/sw/bin/Hyprland";
+      #   };
+      # };
     };
     hyprland = {
       enable = true;
       package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
       portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-      # xwayland.enable = true;
-      # withUWSM = true;
+      withUWSM = true;
     };
   };
 }
