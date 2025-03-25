b6afb05d1db85eff4ab448ad83ed047d96a65e69
Immelancholy <lenalowes0@gmail.com>
2025-03-25 18:01:12 +0000

    switch

--- system/programs/bluetooth.nix
+++ system/programs/bluetooth.nix
@@ -1,5 +1,12 @@
 {
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  hardware.bluetooth = {
+    enable = true; # enables support for Bluetooth
+    bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+      };
+    };
+  };
   services.blueman.enable = true;
 }
