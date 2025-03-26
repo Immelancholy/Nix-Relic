60375bf092dd6ff4de15c90ebf5ae41b4aad4a3f
Immelancholy <lenalowes0@gmail.com>
2025-03-26 12:53:03 +0000

    switch

--- home/programs/scripts/sr.sh
+++ home/programs/scripts/sr.sh
@@ -3,20 +3,27 @@ err="%samplerate%"
 pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
 while :
 do
-	currentRate=$(mpc status %samplerate%)
-	if [ "$currentRate" = "$err" ];
+	state=$(mpc status %state%)
+	if [ "$state" = "paused" ] || [ "$state" = "stopped" ];
 	then
-		while [ "$pwrate" != "0" ]; 
-		do
-			pw-metadata -n settings 0 clock.force-rate 0
-			pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
-		done
-
+		pw-metadata -n settings 0 clock.force-rate 0
+		break
 	fi
+	currentRate=$(mpc status %samplerate%)
+
 	if [ "$currentRate" != "$pwrate" ];
 	then
-		pw-metadata -n settings 0 clock.force-rate "$currentRate"
-		pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
+		if [ "$currentRate" = "$err" ];
+		then
+			while [ "$pwrate" != "0" ]; 
+			do
+				pw-metadata -n settings 0 clock.force-rate 0
+				pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
+			done
+		else
+			pw-metadata -n settings 0 clock.force-rate "$currentRate"
+			pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
+		fi
 	fi
 
 done
