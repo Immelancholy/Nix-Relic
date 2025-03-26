634950d5defc0d39f6cb78fb1e235f2fb7fbfe8f
Immelancholy <lenalowes0@gmail.com>
2025-03-26 21:11:07 +0000

    switch

--- system/programs/default.nix
+++ system/programs/default.nix
@@ -36,8 +36,10 @@
     enable = true;
     wantedBy = ["default.target"];
     path = [
-      "/run/current-system/sw"
+      # "/run/current-system/sw"
       "/home/${user}/.local/share"
+      pkgs.mpc
+      pkgs.pipewire
     ];
     script = ''
       /home/${user}/.local/share/bin/mpdchck.sh
