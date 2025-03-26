f8dea4271b957b8cf9a8e0eac4b79d09c44899ec
Immelancholy <lenalowes0@gmail.com>
2025-03-26 21:36:37 +0000

    switch

--- home/programs/scripts/sr.sh
+++ home/programs/scripts/sr.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+pid=$(pgrep qpwgraph)
 pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
 while :
 do
 	state=$(mpc status %state%)
 	if [ "$state" = "paused" ] || [ "$state" = "stopped" ];
 	then
-		pkill qpwgraph
+		kill $pid
 		pw-metadata -n settings 0 clock.force-rate 0
 		uwsm app -- qpwgraph
 		hyprctl dispatch exec "[workspace 4 silent]" uwsm app -- qpwgraph
@@ -15,7 +16,7 @@ do
 
 	if [ "$currentRate" != "$pwrate" ];
 	then
-		pkill qpwgraph
+		kill $pid
 		pw-metadata -n settings 0 clock.force-rate "$currentRate"
 		pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
 
