859f0e373103f9bd2264f90dcde175616a717dcf
Immelancholy <lenalowes0@gmail.com>
2025-03-26 13:28:41 +0000

    switch

--- home/programs/scripts/sr.sh
+++ home/programs/scripts/sr.sh
@@ -5,15 +5,23 @@ do
 	state=$(mpc status %state%)
 	if [ "$state" = "paused" ] || [ "$state" = "stopped" ];
 	then
+		pkill qpwgraph
 		pw-metadata -n settings 0 clock.force-rate 0
+		uwsm app -- qpwgraph
+		uwsm app -- qpwgraph
 		break
 	fi
 	currentRate=$(mpc status %samplerate%)
 
 	if [ "$currentRate" != "$pwrate" ];
 	then
+		pkill qpwgraph
 		pw-metadata -n settings 0 clock.force-rate "$currentRate"
 		pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
+
+		uwsm app -- qpwgraph
+		uwsm app -- qpwgraph
+
 	fi
 
 done
