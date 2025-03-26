9dff725d51077ef41006cf86b9d4801fc380e6b6
Immelancholy <lenalowes0@gmail.com>
2025-03-26 12:17:09 +0000

    switch

--- home/programs/scripts/sr.sh
+++ home/programs/scripts/sr.sh
@@ -8,7 +8,7 @@ do
 	currentRateCurrent=$(mpc status %samplerate%)
 	if [ "$currentRateCurrent" = "$err" ];
 	then
-		while "$pwrate" != 0 
+		while [ "$pwrate" != "0" ]; 
 		do
 			pw-metadata -n settings 0 clock.force-rate 0
 			pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
