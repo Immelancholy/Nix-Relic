12bd58cca483df21d36d2fa70e3a298e4290158e
Immelancholy <lenalowes0@gmail.com>
2025-03-26 13:59:28 +0000

    switch

--- home/programs/hyprland/hyprconf/execs.nix
+++ home/programs/hyprland/hyprconf/execs.nix
@@ -1,6 +1,7 @@
 {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
+      "systemctl restart --user cliphist cliphist-images nm-applet remmina"
       "uwsm app -- $scr/mpdchck.sh"
       ''uwsm app -- mpvpaper -o "--loop" '*' $HOME/Pictures/wallpapers/Neon-Beast-Girl.mp4''
       # "uwsm app -- wl-paste --type text --watch cliphist store # Stores only image data"
