eee9e687653e21e843a595030e5f22e70af9ae7d
Immelancholy <lenalowes0@gmail.com>
2025-03-26 12:16:17 +0000

    switch

--- home/programs/scripts/sr.sh
+++ home/programs/scripts/sr.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 currentRateNext=$(mpc status %samplerate%)
 err="%samplerate%"
+pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
 rmpc stop
 while :
 do
 	currentRateCurrent=$(mpc status %samplerate%)
 	if [ "$currentRateCurrent" = "$err" ];
 	then
-		pw-metadata -n settings 0 clock.force-rate 0
+		while "$pwrate" != 0 
+		do
+			pw-metadata -n settings 0 clock.force-rate 0
+			pwrate=$(pw-metadata -n settings | grep 'clock.force-rate' | cut -d "'" -f 4)
+		done
+
 	fi
 	if [ "$currentRateCurrent" != "$currentRateNext" ];
 	then
