cae80134d40ccaafdf76b81264f45e58429bfeca
Immelancholy <lenalowes0@gmail.com>
2025-03-28 04:29:40 +0000

    switch

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -18,7 +18,7 @@
       startupNotify = false;
       mimeType = ["text/english" "text/plain" "text/x-makefile" "text/x-c++hdr" "text/x-c++src" "text/x-chdr" "text/x-csrc" "text/x-java" "text/x-moc" "text/x-pascal" "text/x-tcl" "text/x-tex" "application/x-shellscript" "text/x-c" "text/x-c++"];
       settings = {
-        Keywords = ["Text" "Editor"];
+        Keywords = "Text;editor";
         TryExec = "${../scripts/nv.sh}";
       };
     };
