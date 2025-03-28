2b604676f334604b14f8ed8ffc9152b7f54521ea
Immelancholy <lenalowes0@gmail.com>
2025-03-28 04:03:56 +0000

    switch

--- home/programs/nvim/default.nix
+++ home/programs/nvim/default.nix
@@ -4,19 +4,19 @@
   lib,
   ...
 }: {
-  xdg.desktopEntries = {
-    nvim = {
-      name = "Neovim";
-      genericName = "Text Editor";
-      # exec = "${lib.getExe pkgs.kitty} ${../scripts/nv.sh}";
-      exec = "${../scripts/nv.sh}";
-      type = "Application";
-      terminal = true;
-      categories = ["Utility" "TextEditor"];
-      icon = "nvim";
-      mimeType = ["text/english" "text/plain" "text/x-makefile" "text/x-c++hdr" "text/x-c++src" "text/x-chdr" "text/x-csrc" "text/x-java" "text/x-moc" "text/x-pascal" "text/x-tcl" "text/x-tex" "application/x-shellscript" "text/x-c" "text/x-c++"];
-    };
-  };
+  # xdg.desktopEntries = {
+  #   nvim = {
+  #     name = "Neovim";
+  #     genericName = "Text Editor";
+  #     # exec = "${lib.getExe pkgs.kitty} ${../scripts/nv.sh}";
+  #     exec = "${../scripts/nv.sh}";
+  #     type = "Application";
+  #     terminal = true;
+  #     categories = ["Utility" "TextEditor"];
+  #     icon = "nvim";
+  #     mimeType = ["text/english" "text/plain" "text/x-makefile" "text/x-c++hdr" "text/x-c++src" "text/x-chdr" "text/x-csrc" "text/x-java" "text/x-moc" "text/x-pascal" "text/x-tcl" "text/x-tex" "application/x-shellscript" "text/x-c" "text/x-c++"];
+  #   };
+  # };
 
   programs.nixvim = {
     enable = true;
