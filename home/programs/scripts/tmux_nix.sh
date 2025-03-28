4b379e89d49944964c691bd7fd676ffb3ce6bbb2
Immelancholy <lenalowes0@gmail.com>
2025-03-28 23:00:45 +0000

    switch

--- home/programs/scripts/tmux_nix.sh
+++ home/programs/scripts/tmux_nix.sh
@@ -10,13 +10,11 @@ while tmux has-session -t $sesh 2>/dev/null; do
 done
 tmux new -d -s $sesh -c /etc/nixos
 
-tmux send-keys "$SHELL" C-m
-
-tmux send-keys "nv.sh" C-m
-
 tmux new-window -c /etc/nixos 
 
 tmux select-window -t ^ 
+
+tmux send-keys "nv.sh" C-m
 
 tmux attach-session -t $sesh
 
