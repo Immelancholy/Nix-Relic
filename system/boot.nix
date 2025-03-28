d947e251e3ea0f83c291e81c29e7ad8dd3f7245a
Immelancholy <lenalowes0@gmail.com>
2025-03-28 20:21:04 +0000

    switch

--- system/boot.nix
+++ system/boot.nix
@@ -12,9 +12,6 @@
   boot.extraModulePackages = with config.boot.kernelPackages; [
     bbswitch
   ];
-  security.polkit = {
-    enable = true;
-  };
   boot.kernelPackages = pkgs.linuxPackages_zen;
   # boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelParams = [
