87664d6b4bf3cad6e94e113f9d78462a85914e24
Immelancholy <lenalowes0@gmail.com>
2025-03-29 17:36:09 +0000

    switch

--- system/programs/default.nix
+++ system/programs/default.nix
@@ -179,6 +179,14 @@
       };
     };
     extraPackages = with pkgs; [
+      xorg.libXcursor
+      xorg.libXi
+      xorg.libXinerama
+      xorg.libXScrnSaver
+      libpng
+      libpulseaudio
+      libvorbis
+      stdenv.cc.cc.lib
       libkrb5
       keyutils
     ];
