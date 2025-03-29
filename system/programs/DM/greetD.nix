576c186d63922d3b2f5543b3ab04770a9548a172
Immelancholy <lenalowes0@gmail.com>
2025-03-29 16:51:44 +0000

    switch

--- system/programs/DM/greetD.nix
+++ system/programs/DM/greetD.nix
@@ -4,7 +4,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --cmd steam-gamescope";
         user = "greeter";
       };
     };
