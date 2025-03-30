91936b6d15773f3aac1b91e326da5105ad2eed95
Immelancholy <lenalowes0@gmail.com>
2025-03-30 18:27:30 +0100

    switch

--- system/programs/hyprland.nix
+++ system/programs/hyprland.nix
@@ -3,12 +3,10 @@
   inputs,
   ...
 }: {
-
-    hyprland = {
-      enable = true;
-      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-      portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-      withUWSM = true;
-    };
+  hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    withUWSM = true;
   };
 }
