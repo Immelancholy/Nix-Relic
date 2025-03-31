f35b060be60f8f4e882b37b6da7a1eddc2a4f5dc
Immelancholy <lenalowes0@gmail.com>
2025-03-31 04:36:46 +0100

    switch

--- system/programs/steam.nix
+++ system/programs/steam.nix
@@ -47,7 +47,7 @@ in {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
-    extest.enable = true;
+    # extest.enable = true;
     gamescopeSession = {
       enable = true;
     };
