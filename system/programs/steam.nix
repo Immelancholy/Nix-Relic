e23b24b83b97f6ff2273f07aaae1edbb809bc1ce
Immelancholy <lenalowes0@gmail.com>
2025-04-02 21:40:43 +0100

    switch

--- system/programs/steam.nix
+++ system/programs/steam.nix
@@ -28,6 +28,8 @@
     export tty=$(tty)
     export ENABLE_GAMESCCOPE_WSI=0
     export "''${mangoVars[@]}"
+    pw-metadata -n default 0 default.audio.sink '{ "name": "desktop_output" }' 'Spa:String:JSON'
+    pw-metadata -n default 0 default.audio.source '{ "name": "desktop_input" }' 'Spa:String:JSON'
     exec gamescope "''${gamescopeArgs[@]}" -- steam "''${steamArgs[@]}"
   '';
   steamos-session-select = pkgs.writeShellScriptBin "steamos-session-select" ''
