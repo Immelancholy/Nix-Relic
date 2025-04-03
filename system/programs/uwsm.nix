a8ea5e476ecdb9153be01f12c5668432b48b977d
Immelancholy <lenalowes0@gmail.com>
2025-04-03 07:29:45 +0100

    switch

--- system/programs/uwsm.nix
+++ system/programs/uwsm.nix
@@ -1,4 +1,12 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  hyprgame = pkgs.writeScriptBin ''
+    config="$HOME/hyprcord.config"
+    exec Hyprland --config "$config"
+  '';
+in {
+  environment.systemPackages = [
+    hyprgame
+  ];
   programs = {
     uwsm = {
       enable = true;
