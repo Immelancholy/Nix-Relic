db3644fc429db9ac391540b8149a14a0b4790072
Immelancholy <lenalowes0@gmail.com>
2025-04-03 08:27:04 +0100

    switch

--- system/programs/uwsm.nix
+++ system/programs/uwsm.nix
@@ -8,7 +8,12 @@
     env = XDG_CURRENT_DESKTOP,Hyprland
 
     general {
+      gaps_in = 3
+      gaps_out = 8
+      border_size = 2
+
       layout = dwindle
+      resize_on_border = true
     }
 
     dwindle {
