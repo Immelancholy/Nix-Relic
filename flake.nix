23bd22d565577f812baf60534ceb32eeebb67a23
Immelancholy <lenalowes0@gmail.com>
2025-04-04 21:55:14 +0100

    switch

--- flake.nix
+++ flake.nix
@@ -106,7 +106,7 @@
     # hyprMonitor = ", preferred, auto, 1"; # monitor for hyprland to use, leave this default and then edit it in post install by using hyprctl monitors to find your monitor
     hyprMonitor = "HDMI-A-1, 1920x1080@144, 0x0, 1, bitdepth, 8"; #example and also my monitor lol
   in {
-    formatter.system = nixpkgs.alejandra;
+    formatter = nixpkgs.alejandra.system;
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         inherit system;
