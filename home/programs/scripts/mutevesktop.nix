53a970017d0791500c891e7fff8bd4b7ad6a9e68
Immelancholy <lenalowes0@gmail.com>
2025-04-06 13:45:01 +0100

    switch

--- home/programs/scripts/mutevesktop.nix
+++ home/programs/scripts/mutevesktop.nix
@@ -2,13 +2,7 @@
   home.packages = with pkgs; [
     (writeShellScriptBin "pushToMuteVesktop" ''
       input=$(wpctl status | grep "Chromium input" | grep -v "mela@nixos" | grep -o -E '[0-9]+')
-      if [ "$mute" = 1 ]; then
-        wpctl set-mute "$input" 0
-        mute=""
-      else
-        mute=1
-        wpctl set-mute "$input" 1
-      fi
+      wpctl set-mute "$input" toggle
     '')
   ];
 }
