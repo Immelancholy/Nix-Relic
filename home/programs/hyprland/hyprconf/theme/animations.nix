3304450d49348c7a295b991a26202a25e4ed5a22
Immelancholy <lenalowes0@gmail.com>
2025-04-06 19:04:40 +0100

    switch

--- home/programs/hyprland/hyprconf/theme/animations.nix
+++ home/programs/hyprland/hyprconf/theme/animations.nix
@@ -1,23 +1,29 @@
 {
-  wayland.windowManager.hyprland.extraConfig = ''
-    layerrule = noanim, hyprpicker
-    layerrule = noanim, selection
+  wayland.windowManager.hyprland.settings = {
+    layerrule = [
+      "noanim, hyprpicker"
+      "noanim, selection"
+    ];
 
-    animations {
-        enabled = yes
-        bezier = wind, 0.05, 0.9, 0.1, 1.05
-        bezier = winIn, 0.1, 1.1, 0.1, 1.1
-        bezier = winOut, 0.3, -0.3, 0, 1
-        bezier = liner, 1, 1, 1, 1
-        animation = windows, 1, 6, wind, slide
-        animation = windowsIn, 1, 6, winIn, slide
-        animation = windowsOut, 1, 5, winOut, slide
-        animation = windowsMove, 1, 5, wind, slide
-        animation = border, 1, 1, liner
-        animation = borderangle, 1, 35, liner, loop
-        animation = fade, 1, 10, default
-        animation = workspaces, 1, 5, wind
-        animation = specialWorkspace, 1, 5, wind, slidevert
-    }
-  '';
+    animations = {
+      enabled = true;
+      bezier = [
+        "wind, 0.05, 0.9, 0.1, 1.05"
+        "winIn, 0.1, 1.1, 0.1, 1.1"
+        "winOut, 0.3, -0.3, 0, 1"
+        "liner, 1, 1, 1, 1"
+      ];
+      animation = [
+        "windows, 1, 6, wind, slide"
+        "windowsIn, 1, 6, winIn, slide"
+        "windowsOut, 1, 5, winOut, slide"
+        "windowsMove, 1, 5, wind, slide"
+        "border, 1, 1, liner"
+        "borderangle, 1, 35, liner, loop"
+        "fade, 1, 10, default"
+        "workspaces, 1, 5, wind"
+        "specialWorkspace, 1, 5, wind, slidevert"
+      ];
+    };
+  };
 }
--- home/programs/hyprland/hyprconf/theme/theme.nix
+++ home/programs/hyprland/hyprconf/theme/theme.nix
@@ -1,42 +1,42 @@
 {
-  wayland.windowManager.hyprland.extraConfig = ''
-    general {
-      gaps_in = 3
-      gaps_out = 8
-      border_size = 2
-      col.active_border = rgba(cba6f7ff) rgba(89dcebff) rgba(f5e0dcff) 40deg
-      col.inactive_border = rgba(b4befecc) rgba(6c7086cc) 45deg
-      resize_on_border = true
-    }
-    group {
-        col.border_active = rgba(cba6f7ff) rgba(89dcebff) rgba(f5e0dcff) 40deg
-        col.border_inactive = rgba(b4befecc) rgba(6c7086cc) 45deg
-        col.border_locked_active =rgba(cba6f7ff) rgba(89dcebff) rgba(f5e0dcff) 40deg
-        col.border_locked_inactive = rgba(b4befecc) rgba(6c7086cc) 45deg
-    }
+  wayland.windowManager.hyprland.settings = {
+    general = {
+      gaps_in = 3;
+      gaps_out = 8;
+      border_size = 2;
+      "col.active_border" = "rgba(cba6f7ff) rgba(89dcebff) rgba(f5e0dcff) 40deg";
+      "col.inactive_border" = "rgba(b4befecc) rgba(6c7086cc) 45deg";
+      resize_on_border = true;
+    };
+    group = {
+      "col.border_active" = "rgba(cba6f7ff) rgba(89dcebff) rgba(f5e0dcff) 40deg";
+      "col.border_inactive" = "rgba(b4befecc) rgba(6c7086cc) 45deg";
+      "col.border_locked_active" = "rgba(cba6f7ff) rgba(89dcebff) rgba(f5e0dcff) 40deg";
+      "col.border_locked_inactive" = "rgba(b4befecc) rgba(6c7086cc) 45deg";
+    };
 
-    decoration {
-        rounding = 20
-        shadow:enabled = false
+    decoration = {
+      rounding = 20;
+      shadow.enabled = false;
 
-        blur {
-            enabled = yes
-            size = 3
-            passes = 2
-            vibrancy = 0.5
-            new_optimizations = true
-            ignore_opacity = true
-            xray = false
-        }
-    }
+      blur = {
+        enabled = true;
+        size = 3;
+        passes = 2;
+        vibrancy = 0.5;
+        new_optimizations = true;
+        ignore_opacity = true;
+        xray = false;
+        special = true;
+      };
+    };
 
-    layerrule = blur,waybar
+    layerrule = [
+      "blur,waybar"
+    ];
 
-    decoration {
-        dim_special = 0.3
-        blur {
-          special = true
-        }
-    }
-  '';
+    decoration = {
+      dim_special = 0.3;
+    };
+  };
 }
