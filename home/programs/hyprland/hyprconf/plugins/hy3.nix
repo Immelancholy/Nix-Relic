19ae46efb3cc3c4e314a042f369450ca68fd3773
Immelancholy <lenalowes0@gmail.com>
2025-04-06 16:34:41 +0100

    switch

--- home/programs/hyprland/hyprconf/plugins/hy3.nix
+++ home/programs/hyprland/hyprconf/plugins/hy3.nix
@@ -16,46 +16,48 @@ in {
     };
   };
   config = mkIf cfg.enable {
-    plugins = [
-      inputs.hy3.packages.${pkgs.system}.hy3
-    ];
-    settings = {
-      general = {
-        layout = "hy3";
+    wayland.windowManager.hyprland = {
+      plugins = [
+        inputs.hy3.packages.${pkgs.system}.hy3
+      ];
+      settings = {
+        general = {
+          layout = "hy3";
+        };
       };
-    };
-    extraConfig = ''
-      plugin {
-        hy3 {
-          tabs {
-            radius = 10
-            padding = 4
-            text_font = JetBrainsMono Nerd Font Mono Bold # default: Sans
-            text_height = 9
-            border_width = 2
-            col.active = rgba(1e1e2eB5)
-            col.active.border = rgba(cba6f7ff)
-            col.active.text = rgba(cdd6f4ff)
-            col.focused = rgba(1e1e2eB5)
-            col.focused.border = rgba(6c7086cc)
-            col.focused.text = rgba(7f849cff)
-            col.inactive = rgba(1e1e2eB5)
-            col.inactive.border = rgba(b4befecc)
-            col.inactive.text = rgba(cdd6f4ff)
-            col.urgent = rgba(1e1e2eB5)
-            col.urgent.border = rgba(f9e2afff)
-            col.urgent.text = rgba(cdd6f4ff)
-          }
-          # no_gaps_when_only = 1
-          group_inset = 4
-          autotile {
-            enable = true
-            ephemeral_groups = false
-            trigger_width = 640
-            trigger_height = 340
+      extraConfig = ''
+        plugin {
+          hy3 {
+            tabs {
+              radius = 10
+              padding = 4
+              text_font = JetBrainsMono Nerd Font Mono Bold # default: Sans
+              text_height = 9
+              border_width = 2
+              col.active = rgba(1e1e2eB5)
+              col.active.border = rgba(cba6f7ff)
+              col.active.text = rgba(cdd6f4ff)
+              col.focused = rgba(1e1e2eB5)
+              col.focused.border = rgba(6c7086cc)
+              col.focused.text = rgba(7f849cff)
+              col.inactive = rgba(1e1e2eB5)
+              col.inactive.border = rgba(b4befecc)
+              col.inactive.text = rgba(cdd6f4ff)
+              col.urgent = rgba(1e1e2eB5)
+              col.urgent.border = rgba(f9e2afff)
+              col.urgent.text = rgba(cdd6f4ff)
+            }
+            # no_gaps_when_only = 1
+            group_inset = 4
+            autotile {
+              enable = true
+              ephemeral_groups = false
+              trigger_width = 640
+              trigger_height = 340
+            }
           }
         }
-      }
-    '';
+      '';
+    };
   };
 }
