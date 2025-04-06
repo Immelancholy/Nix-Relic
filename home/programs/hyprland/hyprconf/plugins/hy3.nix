f385e5675408cdec5251c12ed0247dc0c09d78e8
Immelancholy <lenalowes0@gmail.com>
2025-04-06 17:23:58 +0100

    switch

--- home/programs/hyprland/hyprconf/plugins/hy3.nix
+++ home/programs/hyprland/hyprconf/plugins/hy3.nix
@@ -16,20 +16,19 @@ in {
     };
   };
   config = mkMerge [
-    (mkIf cfg.useHy3
-      == false {
-        wayland.windowManager.hyprland = {
-          settings = {
-            general = {
-              layout = "dwindle";
-            };
-            dwindle = {
-              pseudotile = true;
-              preserve_split = true;
-            };
+    (mkIf !cfg.useHy3 {
+      wayland.windowManager.hyprland = {
+        settings = {
+          general = {
+            layout = "dwindle";
+          };
+          dwindle = {
+            pseudotile = true;
+            preserve_split = true;
           };
         };
-      })
+      };
+    })
     (mkIf cfg.useHy3
       == true {
         wayland.windowManager.hyprland = {
