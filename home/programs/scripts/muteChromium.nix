b13ed937d0504c1c46b4b39b53ac0ea168e3ba83
Immelancholy <lenalowes0@gmail.com>
2025-04-06 14:12:44 +0100

    switch

--- home/programs/scripts/muteChromium.nix
+++ home/programs/scripts/muteChromium.nix
@@ -3,7 +3,9 @@
     (writeShellScriptBin "pushToMuteChromium" ''
       mapfile -t inputs < <(wpctl status | grep "Chromium input" | grep -v "mela@nixos" | grep -o -E '[0-9]+')
       for i in "''${inputs[@]}"
+      do
         wpctl set-mute "$i" toggle
+      done
     '')
   ];
 }
