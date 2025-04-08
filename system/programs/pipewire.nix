8ac414e22984e5aa7b3d5d443b46f3a3e4970ce6
Immelancholy <lenalowes0@gmail.com>
2025-04-08 15:25:29 +0100

    change input virtual devices to loopback devices

--- system/programs/pipewire.nix
+++ system/programs/pipewire.nix
@@ -18,23 +18,62 @@
       enable = true;
     };
   };
-  services.pipewire.extraConfig.pipewire."default" = {
-    "context.properties" = {
-      "default.audio.sink" = {
-        "name" = "desktop_output";
-      };
-      "default.audio.source" = {
-        "name" = "desktop_input";
-      };
-      "default.configured.audio.sink" = {
-        "name" = "desktop_output";
-      };
-      "default.configured.audio.source" = {
-        "name" = "desktop_input";
-      };
-    };
-  };
   services.pipewire.extraConfig.pipewire."91-null-sinks" = {
+    "context.modules" = [
+      {
+        # properyty when no other driver is currently active. JACK clients need this.
+        name = "libpipewire-module-loopback";
+        args = {
+          "audio.position" = "[ FL FR ]";
+          "capture.props" = {
+            "node.name" = "dummy_plug";
+            "node.description" = "Dummy Plug In";
+            "media.class" = "Audio/Sink";
+          };
+          "playback.props" = {
+            "node.name" = "dummy_plug";
+            "node.description" = "Dummy Plug Out";
+            "media.class" = "Audio/Source";
+          };
+        };
+      }
+      {
+        # properyty when no other driver is currently active. JACK clients need this.
+        name = "libpipewire-module-loopback";
+        args = {
+          "audio.position" = "[ FL FR ]";
+          "capture.props" = {
+            "node.name" = "commes_mic_in";
+            "node.description" = "Commes Mic In";
+            "node.target" = "easyeffects_source";
+            "media.class" = "Audio/Sink";
+          };
+          "playback.props" = {
+            "node.name" = "commes_mic_out";
+            "node.description" = "Commes Mic Out";
+            "media.class" = "Audio/Source";
+          };
+        };
+      }
+      {
+        # properyty when no other driver is currently active. JACK clients need this.
+        name = "libpipewire-module-loopback";
+        args = {
+          "audio.position" = "[ FL FR ]";
+          "capture.props" = {
+            "node.name" = "desktop_mic_in";
+            "node.description" = "Desktop Mic In";
+            "node.target" = "easyeffects_source";
+            "media.class" = "Audio/Sink";
+          };
+          "playback.props" = {
+            "node.name" = "desktop_mic_out";
+            "node.description" = "Desktop Mic Out";
+            "media.class" = "Audio/Source";
+          };
+        };
+      }
+    ];
     "context.objects" = [
       {
         # A default dummy driver. This handles nodes marked with the "node.always-driver"
@@ -54,7 +93,7 @@
           "node.name" = "commes_output";
           "node.description" = "Commes Output";
           "media.class" = "Audio/Sink";
-          "audio.position" = "FL, FR";
+          "audio.position" = "[ FL FR ]";
           "monitor.channel-volumes" = "true";
           "monitor.passthrough" = "true";
           "adapter.auto-port-config" = {
@@ -72,7 +111,7 @@
           "node.name" = "desktop_output";
           "node.description" = "Desktop Output";
           "media.class" = "Audio/Sink";
-          "audio.position" = "FL, FR";
+          "audio.position" = "[ FL FR ]";
           "monitor.channel-volumes" = "true";
           "monitor.passthrough" = "true";
           "adapter.auto-port-config" = {
@@ -90,7 +129,7 @@
           "node.name" = "virtual_cable";
           "node.description" = "Virtual Cable";
           "media.class" = "Audio/Sink";
-          "audio.position" = "FL, FR";
+          "audio.position" = "[ FL FR ]";
           "monitor.channel-volumes" = "true";
           "monitor.passthrough" = "true";
           "adapter.auto-port-config" = {
@@ -108,62 +147,7 @@
           "node.name" = "virtual_cable_b";
           "node.description" = "Virtual Cable B";
           "media.class" = "Audio/Sink";
-          "audio.position" = "FL, FR";
-          "monitor.channel-volumes" = "true";
-          "monitor.passthrough" = "true";
-          "adapter.auto-port-config" = {
-            "mode" = "dsp";
-            "monitor" = "true";
-            "position" = "preserve";
-          };
-        };
-      }
-      {
-        # properyty when no other driver is currently active. JACK clients need this.
-        factory = "adapter";
-        args = {
-          "factory.name" = "support.null-audio-sink";
-          "node.name" = "desktop_input";
-          "node.description" = "Desktop Input";
-          "media.class" = "Audio/Source/Virtual";
-          "audio.position" = "FL, FR";
-          "monitor.channel-volumes" = "true";
-          "monitor.passthrough" = "true";
-          "adapter.auto-port-config" = {
-            "mode" = "dsp";
-            "monitor" = "true";
-            "position" = "preserve";
-          };
-        };
-      }
-      {
-        # properyty when no other driver is currently active. JACK clients need this.
-        factory = "adapter";
-        args = {
-          "factory.name" = "support.null-audio-sink";
-          "node.name" = "commes_input";
-          "node.description" = "Commes Input";
-          "media.class" = "Audio/Source/Virtual";
-          "audio.position" = "FL, FR";
-          "monitor.channel-volumes" = "true";
-          "monitor.passthrough" = "true";
-          "adapter.auto-port-config" = {
-            "mode" = "dsp";
-            "monitor" = "true";
-            "position" = "preserve";
-          };
-        };
-      }
-      {
-        # properyty when no other driver is currently active. JACK clients need this.
-        factory = "adapter";
-        args = {
-          "factory.name" = "support.null-audio-sink";
-          "node.name" = "dummy_plug";
-          "node.description" = "Dummy Plug";
-          "node.virtual" = "true";
-          "media.class" = "Audio/Source/Virtual";
-          "audio.position" = "FL, FR";
+          "audio.position" = "[ FL FR ]";
           "monitor.channel-volumes" = "true";
           "monitor.passthrough" = "true";
           "adapter.auto-port-config" = {
