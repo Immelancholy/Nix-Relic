8884ac58a8c8af100ecfd7aa5fa574fe5ea7b71f
Immelancholy <lenalowes0@gmail.com>
2025-04-10 12:38:27 +0100

    Change Systems

--- Nix-Dotfiles/flake.nix
+++ Nix-Dotfiles/flake.nix
@@ -108,10 +108,7 @@
   } @ inputs: let
     systems = [
       "aarch64-linux"
-      "i686-linux"
       "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
     ];
     forAllSystems = nixpkgs.lib.genAttrs systems;
     system = forAllSystems (system: system);
