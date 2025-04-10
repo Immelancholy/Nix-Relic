175eca524b9224be01b6b78d664d7454117d5a5f
Immelancholy <lenalowes0@gmail.com>
2025-04-10 12:38:01 +0100

    change systems

--- flake.nix
+++ flake.nix
@@ -8,10 +8,7 @@
   outputs = {nixpkgs, ...}: let
     systems = [
       "aarch64-linux"
-      "i686-linux"
       "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
     ];
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
