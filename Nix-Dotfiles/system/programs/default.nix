2921cbb22636a04990af42616e92453f330b4262
Immelancholy <lenalowes0@gmail.com>
2025-04-10 12:28:12 +0100

    Gnome-Keyring

--- Nix-Dotfiles/system/programs/default.nix
+++ Nix-Dotfiles/system/programs/default.nix
@@ -112,7 +112,7 @@
     enableAskPassword = true;
     askPassword = "${pkgs.seahorse}/libexec/seahorse/ssh-askpass";
   };
-  # environment.variables.SSH_ASKPASS_REQUIRE = "prefer";
+  environment.variables.SSH_ASKPASS_REQUIRE = "prefer";
 
   programs.nm-applet = {
     enable = true;
