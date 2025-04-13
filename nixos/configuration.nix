904a6c5a0aa214afbdbb25bfa3574dd4c37f9bbd
Immelancholy <lenalowes0@gmail.com>
2025-04-13 20:00:37 +0100

    package to latest

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -101,7 +101,7 @@
       open = true;
       powerManagement = true;
       finePowerManagement = false;
-      package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+      # package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
       prime = {
         enable = false;
         # intelBusId = ""; # For Intel
