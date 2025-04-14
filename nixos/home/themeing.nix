26c434a741961f3a66b561c36e3279c3499c22ed
Immelancholy <lenalowes0@gmail.com>
2025-04-14 10:36:58 +0100

    Nemo Dconf

--- nixos/home/themeing.nix
+++ nixos/home/themeing.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   catppuccin = {
     flavor = "mocha";
     accent = "mauve";
@@ -86,6 +90,28 @@
     "org/cinnamon/desktop/applications/terminal" = {
       exec = "kitty";
       # exec-arg = ""; # argument
+    };
+    "org/nemo/preferences" = {
+      click-double-parent-folder = true;
+      show-hidden-files = true;
+      show-image-thumbnails = "always";
+      thumbnail-limit = lib.hm.gvariant.mkUint64 104857600;
+    };
+    "org/nemo/preferences/menu-config" = {
+      selection-menu-copy-to = true;
+      selection-menu-move-to = true;
+    };
+    "org/nemo/search" = {
+      search-reverse-sort = false;
+      search-sort-column = "name";
+    };
+    "org/nemo/window-state" = {
+      maximized = true;
+      sidebar-bookmark-breakpoint = 5;
+      sidebar-width = 141;
+      start-with-menu-bar = false;
+      start-with-sidebar = true;
+      start-with-toolbar = true;
     };
   };
 }
