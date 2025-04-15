4bd4aab0840fbb4150e6013271a24660dbd5be67
Immelancholy <lenalowes0@gmail.com>
2025-04-15 13:34:43 +0100

    Delete older than 6 days

--- nixos/system/default.nix
+++ nixos/system/default.nix
@@ -75,7 +75,7 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 7d";
+    options = "--delete-older-than 6d";
   };
   nix.settings.auto-optimise-store = true;
   # ...
