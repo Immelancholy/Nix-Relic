48674a5e066354a79eb43debeb77dd065e9292d1
Immelancholy <lenalowes0@gmail.com>
2025-05-07 15:44:02 +0100

    Volume Control

--- Nix-Relic/configuration.nix
+++ Nix-Relic/configuration.nix
@@ -80,6 +80,7 @@
     #   name = ""; #MPRIS name of the player (defaults to mpd)
     #   cmd = ""; # command to launch music player (defaults to mpd)
     #   class = ""; # wmclass of music player (defaults to mpd)
+    #   scriptUseDefaultSink = true; #Use default sink for vol control script (defaults to false)
     # };
 
     # Important hyprland user configs
