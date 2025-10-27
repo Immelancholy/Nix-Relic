90b3346e8daef3a466480903ab031c6d68f03f3d
Immelancholy <lenalowes0@gmail.com>
2025-10-27 12:47:49 +0000

    feat: configure animated wallpaper and update flake
    This commit introduces animated wallpaper support and updates the Nix flake lock file.
    
    - Configures an animated wallpaper using `Evil_Miku.mp4`.
    - Updates the `flake.lock` file to reflect the latest changes in the `Nix-Relic-Modules` repository.

--- Nix-Relic/configuration.nix
+++ Nix-Relic/configuration.nix
@@ -4,122 +4,127 @@
   config,
   ...
 }: {
-  nix-relic.users.users = {
-    your-user = {
-      isNormalUser = true;
-      isAdmin = true; #enable or disable this to give or take sudo access.
-      home-config = {
-        imports = [
-          ./nixos/home/fetch_images.nix
-        ];
+  nix-relic = {
+    wallpaper = {
+      path = "./nixos/home/backgrounds/Evil_Miku.png";
+      animatedWallpaper = {
+        enable = true;
+        path = "./nixos/home/backgrounds/Evil_Miku.mp4";
+      };
+    };
+    users.users = {
+      your-user = {
+        isNormalUser = true;
+        isAdmin = true; #enable or disable this to give or take sudo access.
+        home-config = {
+          imports = [
+            ./nixos/home/fetch_images.nix
+          ];
 
-        catppuccin = {
-          flavor = "mocha";
-          accent = "mauve";
-        };
-
-        # stylix.targets = {
-        #   # comment this if you don't want catppuccin theme, I made a module to autodisable their respective catppuccin themes.
-        #   tmux.enable = false;
-        #   yazi.enable = false;
-        #   btop.enable = false;
-        #   spicetify.enable = false;
-        #   kitty.enable = false;
-        #   nixvim.enable = false;
-        #   zen.enable = false;
-        #   vesktop.enable = false;
-        # };
-
-        programs.vesktop = {
-          enable = true;
-        };
-
-        programs.git = {
-          enable = true;
-          userName = ""; # username for git
-          userEmail = ""; # email for git
-          extraConfig = {
-            init = {
-              defaultBranch = "main";
-            };
+          catppuccin = {
+            flavor = "mocha";
+            accent = "mauve";
           };
-        };
 
-        services.mpd = {
-          extraConfig = ''
-            audio_output {
-              type  "pipewire"
-              name  "Pipewire Sound Server"
-              target  "easyeffects_sink"
-            }
-          '';
-        };
+          # stylix.targets = {
+          #   # Uncomment this if you want to use catppuccin theme, I made a module to autodisable their respective catppuccin themes.
+          #   tmux.enable = false;
+          #   yazi.enable = false;
+          #   btop.enable = false;
+          #   spicetify.enable = false;
+          #   kitty.enable = false;
+          #   nixvim.enable = false;
+          #   zen.enable = false;
+          #   vesktop.enable = false;
+          # };
 
-        player = {
-          enable = true;
-          # name = ""; #MPRIS name of the player (defaults to mpd)
-          # cmd = ""; # command to launch music player (defaults to mpd)
-          # class = ""; # wmclass of music player (defaults to mpd)
-          # scriptUseDefaultSink = true; #Use default sink for vol control script (defaults to false)
-        };
-
-        # Important hyprland user configs
-        wayland.windowManager.hyprland = {
-          # ONLY ENABLE 1 LAYOUT!!
-          layout = {
-            master.enable = false;
-            dwindle.enable = false;
-            hy3.enable = true;
-          };
-          useHyprspace = true;
-          liveWallpaper = {
+          programs.vesktop = {
             enable = true;
-            path = ./nixos/home/backgrounds/Evil_Miku.mp4;
           };
-          settings = {
-            cursor = {
-              no_hardware_cursors = false;
-            };
-            monitor = ", preferred, auto, 1";
-            input = {
-              kb_layout = "gb";
-              follow_mouse = "1";
 
-              sensitivity = "0";
-              force_no_accel = "1";
-              numlock_by_default = "true";
+          programs.git = {
+            enable = true;
+            userName = ""; # username for git
+            userEmail = ""; # email for git
+            extraConfig = {
+              init = {
+                defaultBranch = "main";
+              };
             };
-            bind = [
-              # "$mod, F9, pass, class:^(com.obsproject.Studio)$"
-              # "$mod, F10, pass, class:^(com.obsproject.Studio)$"
-              # "$mod, F12, pass, class:^(com.obsproject.Studio)$"
-            ];
           };
-        };
 
-        home.sessionVariables = {
-          NOTES_PATH = ""; # path to notes folder ( for neovim )
-          PROJECTS_PATH = ""; # path to Projects folder ( for neovim )
-        };
+          services.mpd = {
+            extraConfig = ''
+              audio_output {
+                type  "pipewire"
+                name  "Pipewire Sound Server"
+                target  "easyeffects_sink"
+              }
+            '';
+          };
 
-        home.packages = with pkgs; [
-          # Packages for this user only here
-        ];
+          player = {
+            enable = true;
+            # name = ""; #MPRIS name of the player (defaults to mpd)
+            # cmd = ""; # command to launch music player (defaults to mpd)
+            # class = ""; # wmclass of music player (defaults to mpd)
+            # scriptUseDefaultSink = true; #Use default sink for vol control script (defaults to false)
+          };
 
-        programs.nixvim = {
-          enable = true;
-          defaultEditor = true;
-          plugins = {
-            obsidian = {
-              enable = false;
-              settings = {
-                ui.enable = false;
-                workspaces = [
-                  {
-                    name = "";
-                    path = "";
-                  }
-                ];
+          # Important hyprland user configs
+          wayland.windowManager.hyprland = {
+            # ONLY ENABLE 1 LAYOUT!!
+            layout = {
+              master.enable = false;
+              dwindle.enable = false;
+              hy3.enable = true;
+            };
+            useHyprspace = true;
+            settings = {
+              cursor = {
+                no_hardware_cursors = false;
+              };
+              monitor = ", preferred, auto, 1";
+              input = {
+                kb_layout = "gb";
+                follow_mouse = "1";
+
+                sensitivity = "0";
+                force_no_accel = "1";
+                numlock_by_default = "true";
+              };
+              bind = [
+                # "$mod, F9, pass, class:^(com.obsproject.Studio)$"
+                # "$mod, F10, pass, class:^(com.obsproject.Studio)$"
+                # "$mod, F12, pass, class:^(com.obsproject.Studio)$"
+              ];
+            };
+          };
+
+          home.sessionVariables = {
+            NOTES_PATH = ""; # path to notes folder ( for neovim )
+            PROJECTS_PATH = ""; # path to Projects folder ( for neovim )
+          };
+
+          home.packages = with pkgs; [
+            # Packages for this user only here
+          ];
+
+          programs.nixvim = {
+            enable = true;
+            defaultEditor = true;
+            plugins = {
+              obsidian = {
+                enable = false;
+                settings = {
+                  ui.enable = false;
+                  workspaces = [
+                    {
+                      name = "";
+                      path = "";
+                    }
+                  ];
+                };
               };
             };
           };
@@ -127,17 +132,14 @@
       };
     };
   };
+  stylix = {
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml"; # Stylix theme, Uncomment this to use a base-16 scehme, in this case Catppuccin.
+  };
+
   networking.hostName = "nix-relic";
   programs.weylus.users = [
     "users you want to have access to weylus"
   ];
-
-  stylix = {
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml"; # Stylix theme, comment this to generate theme based on wallpaper
-    polarity = "dark";
-
-    image = ./nixos/home/backgrounds/Evil_Miku.png;
-  };
 
   services.pipewire.extraConfig.pipewire."92-low-latency" = {
     "context.properties" = {
@@ -200,10 +202,6 @@
       enable = false; #logout doesn't work right forcing you to do systemctl restart display-manager to get sddm to pop up again after logging out
       screenWidth = "1920";
       screenHeight = "1080";
-      animatedBackground = {
-        enable = true;
-        path = ./nixos/home/backgrounds/Evil_Miku.mp4;
-      };
     };
     tuiGreet.enable = true;
   };
