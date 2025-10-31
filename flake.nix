4ca89227a417ffab06af89a01affa7de789b91ed
Immelancholy <lenalowes0@gmail.com>
2025-10-31 09:25:28 +0000

    chore: remove redundant nixpkgs follow from hyprquickshot
    The `flake.nix` file explicitly states `nixpkgs.follows = "nixpkgs"` for the `hyprquickshot` input. This is the default behavior and therefore redundant. Removing it simplifies the configuration.

--- flake.nix
+++ flake.nix
@@ -10,7 +10,6 @@
     };
     hyprquickshot = {
       url = "github:Immelancholy/hyprquickshot/open-screenshot-in-satty";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     artis = {
       url = "github:Immelancholy/artis";
