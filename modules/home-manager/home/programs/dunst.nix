c9232dd515e4c0f80d90627c79d411a7e96f5ed7
Immelancholy <lenalowes0@gmail.com>
2025-11-01 16:47:42 +0000

    style(dunst): change default font
    Updates the default font used by Dunst from JetBrainsMono to CaskaydiaCove.

--- modules/home-manager/home/programs/dunst.nix
+++ modules/home-manager/home/programs/dunst.nix
@@ -38,7 +38,8 @@ in {
         padding = 15;
         horizontal_padding = 10;
         max_icon_size = 256;
-        font = "JetBrainsMono Nerd Font Mono 10";
+        # font = "JetBrainsMono Nerd Font Mono 10";
+        font = "CaskaydiaCove Nerd Font Mono 10";
         mouse_left_click = "do_action, close_current";
         mouse_middle_click = "close_current";
         dmenu = "rofi -dmenu";
