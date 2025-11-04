cee402e277bb0b525d416edbed7ae1f5d63aa65e
Immelancholy <lenalowes0@gmail.com>
2025-11-04 09:12:44 +0000

    feat: add mouse bindings for hypr tasking and expo
    Adds middle, scroll up, and scroll down mouse button bindings to reset submaps in hyprtasking and hyprexpo plugins. This allows users to exit submaps using mouse gestures.

--- modules/home-manager/wayland/windowManager/hyprland/plugins/hyprtasking.nix
+++ modules/home-manager/wayland/windowManager/hyprland/plugins/hyprtasking.nix
@@ -67,6 +67,9 @@ in {
           bind = , Escape, hyprtasking:toggle, cursor
           bind = , Space, submap, reset
           bind = , Escape, submap, reset
+          bindmn = mouse:272, submap, reset
+          bindmn = mouse:273, submap, reset
+          bindmn = mouse:274, submap, reset
           submaps = reset
         '';
         plugins = [
