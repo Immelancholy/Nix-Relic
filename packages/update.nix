26859535acff49bcac704301f2d1071f1f89b3e3
Immelancholy <lenalowes0@gmail.com>
2025-11-04 10:55:16 +0000

    fix(packages): enhance NixOS update script
    - Add `withToken` option for flake updates using GitHub CLI token.
    - Set default `flakePath` and `host` values.
    - Improve error handling and user feedback during the update process.

--- packages/update.nix
+++ packages/update.nix
@@ -1,63 +1,69 @@
 {
   writeShellScriptBin,
-  flakePath ? "",
-  host ? "",
+  flakePath ? "/etc/nixos",
+  host ? "nix-relic",
+  withToken ? false,
   ...
-}:
-writeShellScriptBin "update-system" ''
-  onefetch_img () {
-    image="$(find ~/Pictures/fastfetch_logos/ -name "*.jpg" -o -name "*.png" 2> /dev/null | shuf -n1)"
-    if [ "$image" ]; then
-      onefetch --image-protocol kitty -i "$image"
-    else
-      onefetch
-    fi
-  }
-  update () {
-    builtin cd "${flakePath}" || return
-    clear
-    onefetch_img
-    nix flake update --commit-lock-file
-    sudo nixos-rebuild boot --flake .#${host} |& nom
+}: let
+  flakeUpdateCmd =
+    if withToken == true
+    then ''nix flake update --option access-tokens "github.com=$(gh auth token) --commit-lock-file"''
+    else "nix flake update --commit-lock-file";
+in
+  writeShellScriptBin "update-system" ''
+    onefetch_img () {
+      image="$(find ~/Pictures/fastfetch_logos/ -name "*.jpg" -o -name "*.png" 2> /dev/null | shuf -n1)"
+      if [ "$image" ]; then
+        onefetch --image-protocol kitty -i "$image"
+      else
+        onefetch
+      fi
+    }
+    update () {
+      builtin cd "${flakePath}" || return
+      clear
+      onefetch_img
+      ${flakeUpdateCmd}
+      sudo nixos-rebuild boot --flake .#${host} |& nom
 
-    if [ $? -eq 0 ]; then
-      booted="$(readlink /run/booted-system/{initrd,kernel,kernel-modules})"
-      built="$(readlink /nix/var/nix/profiles/system/{initrd,kernel,kernel-modules})"
+      if [ $? -eq 0 ]; then
+        booted="$(readlink /run/booted-system/{initrd,kernel,kernel-modules})"
+        built="$(readlink /nix/var/nix/profiles/system/{initrd,kernel,kernel-modules})"
 
-      if [ "''${booted}" = "''${built}" ]; then
-        sudo nixos-rebuild switch --flake .#${host} |& nom
+        if [ "''${booted}" = "''${built}" ]; then
+          sudo nixos-rebuild switch --flake .#${host} |& nom
+        fi
+
+        echo "Updates Complete!"
+        sleep 1
+        read -n 1 -p 'Press any key to continue...'
+        exit 0
       fi
 
-      echo "Updates Complete!"
+      echo "Updates failed!"
       sleep 1
       read -n 1 -p 'Press any key to continue...'
-      exit 0
-    fi
+      exit 1
 
-    echo "Updates failed!"
-    sleep 1
-    read -n 1 -p 'Press any key to continue...'
-    exit 1
+    }
 
-  }
+    while true; do
 
-  while true; do
+      read -p "Would you like to update your system? [Y/n]" yn
 
-    read -p "Would you like to update your system? [Y/n]" yn
+      if [ $yn == "" ]; then
+        $yn="y"
+      fi
 
-    if [ $yn == "" ]; then
-      $yn="y"
-    fi
+      case $yn in
+          [yY] ) echo "Updating System...";
+              break;;
+          [nN] ) echo "Exiting...";
+              exit;;
+          * ) echo "Invalid Response";;
+      esac
 
-    case $yn in
-        [yY] ) echo "Updating System...";
-            break;;
-        [nN] ) echo "Exiting...";
-            exit;;
-        * ) echo "Invalid Response";;
-    esac
+    done
+    update
 
-  done
-  update
-
-''
+  ''
