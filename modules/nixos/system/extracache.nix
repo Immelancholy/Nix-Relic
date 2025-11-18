b5f42f9ea0dd4964e6128fc54ec23a62b925a415
Immelancholy <lenalowes0@gmail.com>
2025-11-18 16:13:05 +0000

    chore: remove prismlauncher from extra-nix-cache

--- modules/nixos/system/extracache.nix
+++ modules/nixos/system/extracache.nix
@@ -11,13 +11,11 @@
     substituters = [
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
-      "https://prismlauncher.cachix.org"
     ];
 
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "prismlauncher.cachix.org-1:9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c="
     ];
   };
 }
