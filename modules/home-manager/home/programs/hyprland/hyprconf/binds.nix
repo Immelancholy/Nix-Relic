19338c7216d93d577bbb5a7270fae16f983c7d5c
Immelancholy <lenalowes0@gmail.com>
2025-11-30 21:01:23 +0000

    fix(hyprland): Correct submap definition syntax
    The Hyprland submap definition was incorrect. This commit adjusts the
    `player` submap to use the `settings` attribute for its binds.

--- modules/home-manager/home/programs/hyprland/hyprconf/binds.nix
+++ modules/home-manager/home/programs/hyprland/hyprconf/binds.nix
@@ -22,9 +22,9 @@ in {
   home.packages = [
     launches
   ];
-  wayland.windowManager.hyprland.settings = {
+  wayland.windowManager.hyprland = {
     submaps = {
-      player = {
+      player.settings = {
         bind = [
           ", escape, submap, reset"
           "$mod, P, submap, reset"
@@ -40,72 +40,74 @@ in {
         ];
       };
     };
-    bindm = [
-      "$mod, mouse:272, movewindow"
-      "$mod, mouse:273, resizewindow"
-    ];
-    bind =
-      [
-        "$mod, B, exec, binds.sh"
-        "$mod, W, togglefloating"
-        ", Pause, exec, $playerctl play-pause"
-        "$mod, T, exec, $term"
-        ''$mods, T, exec, $term --class "tmux" tmux''
-        "$mod, F, exec, $browser"
-        "$mod, E, exec, $files"
-        "$mod, grave, exec, $menu"
-        "$mod, C, exec, $edit"
-        "Alt, Return, fullscreen"
-        "Alt+Shift, Return, fullscreen, 1"
-        "Alt, Tab, exec, rofi -show window -modi window"
-        "$mods, U, exec, launches"
-        "$mod, Delete, exec, rofi -show power-menu -modi power-menu:rofi-power-menu"
-        "$mod, 0, workspace, 10"
-        "Ctrl+Shift, L, exec, uwsm-app -- swaylock -fF"
-        ''$mod, V, exec, $term --class "NixOSConf" tnix''
-        "$mod, N, exec, rofi -show Cliphist -modi Cliphist:cliphist.sh"
-        "Ctrl, Period, exec, rofi -show emoji nerdy -modi emoji,nerdy"
-        "$mod, G, exec, hyprgame"
-        "$mod, L, workspace, r+1"
-        "$mod, H, workspace, r-1"
-        "$mod, J, workspace, empty"
-        "$mod, Right, workspace, r+1"
-        "$mod, Left, workspace, r-1"
-        "$mod, Down, workspace, empty"
-        "$mod, mouse_up, workspace, e+1"
-        "$mod, mouse_down, workspace, e-1"
-        "$mods, mouse_up, workspace, r+1"
-        "$mods, mouse_down, workspace, r-1"
-        ''Ctrl+Shift, Escape, exec, uwsm app -- $term --title "btop" btop''
-        ", XF86Calculator, exec, uwsm-app -- qalculate-gtk"
-        "$mod, X, togglespecialworkspace"
-        ", Print, exec, hyprquickshot"
-        "$mod, O, exec, uwsm-app -- hyprpicker -a"
-        ''$mods, P, submap, player''
-      ]
-      ++ (
-        # workspaces
-        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-        builtins.concatLists (builtins.genList (
-            i: let
-              ws = i + 1;
-            in [
-              "$mod, code:1${toString i}, workspace, ${toString ws}"
-            ]
-          )
-          9)
-      );
-    bindl = [
-      ", XF86AudioPlay, exec, $playerctl play-pause #pause-play media"
-      ", XF86AudioNext, exec, $playerctl next" #next
-      ", XF86AudioPrev, exec, $playerctl previous" #previous,
-      ", XF86AudioMute, exec, uwsm-app -- playerVol mute" # decrease volume of mpd
-    ];
-    bindel = [
-      ", XF86AudioLowerVolume, exec, uwsm-app -- playerVol dec" # decrease volume of mpd
-      ", XF86AudioRaiseVolume, exec, uwsm-app -- playerVol inc" # increase volume of mpd
-      ", XF86MonBrightnessUp, exec, brightness inc"
-      ", XF86MonBrightnessDown, exec, brightness dec"
-    ];
+    settings = {
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
+      bind =
+        [
+          "$mod, B, exec, binds.sh"
+          "$mod, W, togglefloating"
+          ", Pause, exec, $playerctl play-pause"
+          "$mod, T, exec, $term"
+          ''$mods, T, exec, $term --class "tmux" tmux''
+          "$mod, F, exec, $browser"
+          "$mod, E, exec, $files"
+          "$mod, grave, exec, $menu"
+          "$mod, C, exec, $edit"
+          "Alt, Return, fullscreen"
+          "Alt+Shift, Return, fullscreen, 1"
+          "Alt, Tab, exec, rofi -show window -modi window"
+          "$mods, U, exec, launches"
+          "$mod, Delete, exec, rofi -show power-menu -modi power-menu:rofi-power-menu"
+          "$mod, 0, workspace, 10"
+          "Ctrl+Shift, L, exec, uwsm-app -- swaylock -fF"
+          ''$mod, V, exec, $term --class "NixOSConf" tnix''
+          "$mod, N, exec, rofi -show Cliphist -modi Cliphist:cliphist.sh"
+          "Ctrl, Period, exec, rofi -show emoji nerdy -modi emoji,nerdy"
+          "$mod, G, exec, hyprgame"
+          "$mod, L, workspace, r+1"
+          "$mod, H, workspace, r-1"
+          "$mod, J, workspace, empty"
+          "$mod, Right, workspace, r+1"
+          "$mod, Left, workspace, r-1"
+          "$mod, Down, workspace, empty"
+          "$mod, mouse_up, workspace, e+1"
+          "$mod, mouse_down, workspace, e-1"
+          "$mods, mouse_up, workspace, r+1"
+          "$mods, mouse_down, workspace, r-1"
+          ''Ctrl+Shift, Escape, exec, uwsm app -- $term --title "btop" btop''
+          ", XF86Calculator, exec, uwsm-app -- qalculate-gtk"
+          "$mod, X, togglespecialworkspace"
+          ", Print, exec, hyprquickshot"
+          "$mod, O, exec, uwsm-app -- hyprpicker -a"
+          ''$mods, P, submap, player''
+        ]
+        ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+          builtins.concatLists (builtins.genList (
+              i: let
+                ws = i + 1;
+              in [
+                "$mod, code:1${toString i}, workspace, ${toString ws}"
+              ]
+            )
+            9)
+        );
+      bindl = [
+        ", XF86AudioPlay, exec, $playerctl play-pause #pause-play media"
+        ", XF86AudioNext, exec, $playerctl next" #next
+        ", XF86AudioPrev, exec, $playerctl previous" #previous,
+        ", XF86AudioMute, exec, uwsm-app -- playerVol mute" # decrease volume of mpd
+      ];
+      bindel = [
+        ", XF86AudioLowerVolume, exec, uwsm-app -- playerVol dec" # decrease volume of mpd
+        ", XF86AudioRaiseVolume, exec, uwsm-app -- playerVol inc" # increase volume of mpd
+        ", XF86MonBrightnessUp, exec, brightness inc"
+        ", XF86MonBrightnessDown, exec, brightness dec"
+      ];
+    };
   };
 }
