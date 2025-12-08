85c82783841ad3669d3177542bbb28f3a084904a
Immelancholy <lenalowes0@gmail.com>
2025-12-08 01:13:23 +0000

    feat: add uwsm and hyprland modules
    Introduces new NixOS modules for UWSM (Universal Wayland Session Manager)
    and Hyprland.
    
    - `uwsm.nix`: Provides a generic module for configuring UWSM-managed
      Wayland compositors, including desktop entry generation.
    - `hyprland.nix`: Integrates Hyprland with the UWSM module.

--- modules/nixos/programs/default.nix
+++ modules/nixos/programs/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
     ./steam
+    ./hyprland.nix
   ];
 }
---
+++ modules/nixos/programs/hyprland.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+}: let
+  cfg = config.programs.hyprland;
+in {
+  config = lib.mkIf cfg.withUWSM {
+    programs.uwsm.waylandCompositors = {
+      hyprland = {
+        prettyName = "Hyprland";
+        comment = "Hyprland compositor managed by UWSM";
+        binPath = "/run/current-system/sw/bin/start-hyprland";
+        extraUWSMArgs = [
+          "-D Hyprland"
+        ];
+      };
+    };
+  };
+}
