ccd9740fa10db52eed91aa7f7ac383932238c336
Immelancholy <lenalowes0@gmail.com>
2025-12-09 19:25:26 +0000

    fix: update flake inputs
    - Update `hy3` to `update-to-m_reserved_area-test` branch.
    - Update `hyprland` and `hyprland-plugins` to their latest revisions.

--- flake.nix
+++ flake.nix
@@ -71,7 +71,7 @@
     };
     hy3 = {
       # url = "github:outfoxxed/hy3";
-      url = "github:Immelancholy/hy3/update-to-m_reserved_area";
+      url = "github:Immelancholy/hy3/update-to-m_reserved_area-test";
       inputs.hyprland.follows = "hyprland";
     };
     hyprtasking = {
