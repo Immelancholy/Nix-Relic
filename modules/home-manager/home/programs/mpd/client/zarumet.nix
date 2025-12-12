179b38b86662df234083b005f6a2321fb14a452c
Immelancholy <lenalowes0@gmail.com>
2025-12-12 21:29:27 +0000

    feat: integrate zarumet MPD client via nix-relic
    Adds zarumet as an MPD client, configured to use stylix colors.
    Imports zarumet's home-manager module from nix-relic.

--- modules/home-manager/home/programs/mpd/client/zarumet.nix
+++ modules/home-manager/home/programs/mpd/client/zarumet.nix
@@ -1,9 +1,16 @@
-{config, ...}: let
+{
+  config,
+  inputs,
+  ...
+}: let
   text = "#${config.lib.stylix.colors.base05}";
   red = "#${config.lib.stylix.colors.base08}";
   teal = "#${config.lib.stylix.colors.base0C}";
   flamingo = "#${config.lib.stylix.colors.base0F}";
 in {
+  imports = [
+    inputs.nix-relic.inputs.zarumet.homeModules.default
+  ];
   programs.zarumet = {
     enable = true;
     settings = {
