141a9493f40a7c527ab1e75a67f7f5373ea87e4e
Immelancholy <lenalowes0@gmail.com>
2025-12-19 12:38:49 +0000

    fix: Remove zarumet module configuration
    - Replaced full module import with simple package installation.
    - This resolves issues with zarumet not starting.

--- modules/home-manager/home/programs/mpd/client/zarumet.nix
+++ modules/home-manager/home/programs/mpd/client/zarumet.nix
@@ -1,29 +1,34 @@
-{
-  config,
-  inputs,
-  ...
-}: let
-  text = "#${config.lib.stylix.colors.base05}";
-  red = "#${config.lib.stylix.colors.base08}";
-  teal = "#${config.lib.stylix.colors.base0C}";
-  flamingo = "#${config.lib.stylix.colors.base0F}";
-in {
-  imports = [
-    inputs.nix-relic.inputs.zarumet.homeModules.default
+# {
+#   config,
+#   inputs,
+#   ...
+# }: let
+#   text = "#${config.lib.stylix.colors.base05}";
+#   red = "#${config.lib.stylix.colors.base08}";
+#   teal = "#${config.lib.stylix.colors.base0C}";
+#   flamingo = "#${config.lib.stylix.colors.base0F}";
+# in {
+#   imports = [
+#     inputs.nix-relic.inputs.zarumet.homeModules.default
+#   ];
+#   programs.zarumet = {
+#     enable = true;
+#     settings = {
+#       mpd = {
+#         address = "${config.services.mpd.network.listenAddress}:${builtins.toString config.services.mpd.network.port}";
+#       };
+#       colors = {
+#         border = text;
+#         title = text;
+#         album = flamingo;
+#         artist = red;
+#         status = teal;
+#       };
+#     };
+#   };
+# }
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    zarumet
   ];
-  programs.zarumet = {
-    enable = true;
-    settings = {
-      mpd = {
-        address = "${config.services.mpd.network.listenAddress}:${builtins.toString config.services.mpd.network.port}";
-      };
-      colors = {
-        border = text;
-        title = text;
-        album = flamingo;
-        artist = red;
-        status = teal;
-      };
-    };
-  };
 }
