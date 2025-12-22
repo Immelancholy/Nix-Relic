4b9367bf06724b187d846df3c84da946c4cb80fe
Immelancholy <lenalowes0@gmail.com>
2025-12-22 19:45:21 +0000

    refactor: update zarumet input to specific branch
    Switches the zarumet flake input to the `rmpc-like-lazyload` branch.

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zarumet = {
-      url = "github:Immelancholy/zarumet";
+      url = "github:Immelancholy/zarumet/rmpc-like-lazyload";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zsh-256color = {
