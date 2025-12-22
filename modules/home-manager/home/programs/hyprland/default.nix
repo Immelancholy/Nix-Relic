428d6567eb3aa8c431cac84216d872265954add5
Immelancholy <lenalowes0@gmail.com>
2025-12-22 20:54:33 +0000

    refactor(hyprland): change kill signal from 9 to 15
    Updates the hyprctl signalwindow commands in hyprgame to use SIGTERM (15) instead of SIGKILL (9) for a cleaner shutdown of background windows.

--- modules/home-manager/home/programs/hyprland/default.nix
+++ modules/home-manager/home/programs/hyprland/default.nix
@@ -62,15 +62,15 @@ in {
     (nr.hyprgame.override {
       wallpaper = cfg.liveWallpaper.path;
       extraKills = ''
-        hyprctl dispatch signalwindow 'class:(${playerClass}),9'
-          hyprctl dispatch signalwindow 'class:(neo),9'
-          hyprctl dispatch signalwindow 'class:(fastfetch),9'
-          hyprctl dispatch signalwindow 'class:(btop),9'
-          hyprctl dispatch signalwindow 'class:(cava),9'
+        hyprctl dispatch signalwindow 'class:(${playerClass}),15'
+          hyprctl dispatch signalwindow 'class:(neo),15'
+          hyprctl dispatch signalwindow 'class:(fastfetch),15'
+          hyprctl dispatch signalwindow 'class:(btop),15'
+          hyprctl dispatch signalwindow 'class:(cava),15'
           hyprctl dispatch exec '[workspace 1 silent; float; size 1118 710; move 401 145] ${playerCmdGame}'
       '';
       extraLaunch = ''
-        hyprctl dispatch signalwindow 'class:(${playerClass}),9'
+        hyprctl dispatch signalwindow 'class:(${playerClass}),15'
           hyprctl dispatch exec '[workspace 1 silent; float; size 888 462; move 610 609] uwsm app -- kitty --class "cava" cava.sh'
           hyprctl dispatch exec '[workspace 1 silent; float; size 590 637; move 10 433] uwsm app -- kitty --class "btop" btop.sh'
           hyprctl dispatch exec '[workspace 1 silent; float; size 402 1030; move 1508 42]  uwsm app -- kitty --class "neo" neo.sh'
