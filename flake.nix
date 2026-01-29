b77821fdf2bb83b17f6429363f748adf3e183ed3
Immelancholy <lenalowes0@gmail.com>
2026-01-29 02:22:20 +0000

    chore: remove neovim-nightly and nixvim inputs
    Removes Neovim-related overlays and the Nixvim flake from the configuration.

--- flake.nix
+++ flake.nix
@@ -42,14 +42,6 @@
       url = "github:0xc000022070/zen-browser-flake/beta";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
-      # url = "github:nix-community/nixvim/nixos-24.11";
-
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     hyprland.url = "github:hyprwm/Hyprland";
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
