9fae5bc219208308b946f93ff8021b874230e94f
Immelancholy <lenalowes0@gmail.com>
2026-03-15 17:09:04 +0000

    fix(hyprland): correct syntax error in easymotion.nix
    Fixes a logic error in the `mkIf` statement where the configuration
    was incorrectly combined with a boolean using `&&` outside of the
    function call. Includes minor indentation adjustments.

--- modules/home-manager/wayland/windowManager/hyprland/plugins/easymotion.nix
+++ modules/home-manager/wayland/windowManager/hyprland/plugins/easymotion.nix
@@ -16,31 +16,30 @@ in {
       description = ''Use easymotion'';
     };
   };
-  config =
-    mkIf cfg.easymotion.enable
-    && cfg.usingFlake {
-      wayland.windowManager.hyprland = {
-        plugins = [
-          nr.hyprland-easymotion.packages.${pkgs.stdenv.hostPlatform.system}.hyprland-easymotion
+  config = mkIf (cfg.easymotion.enable
+    && cfg.usingFlake) {
+    wayland.windowManager.hyprland = {
+      plugins = [
+        nr.hyprland-easymotion.packages.${pkgs.stdenv.hostPlatform.system}.hyprland-easymotion
+      ];
+      settings = {
+        bind = [
+          "$mod, I, easymotion, action:hyprctl dispatch focuswindow address:{}"
         ];
-        settings = {
-          bind = [
-            "$mod, I, easymotion, action:hyprctl dispatch focuswindow address:{}"
-          ];
 
-          plugin = {
-            easymotion = {
-              textcolor = "rgba(${config.lib.stylix.colors.base05}ff)";
-              bgcolor = "rgba(${config.lib.stylix.colors.base00}bb)";
-              bordercolor = "rgba($mauveff) rgba($tealff) rgba($rosewaterff) 40deg";
-              blur = 1;
-              textpadding = 8;
-              textsize = 35;
-              bordersize = 2;
-              fullscreen_action = "maximize";
-            };
+        plugin = {
+          easymotion = {
+            textcolor = "rgba(${config.lib.stylix.colors.base05}ff)";
+            bgcolor = "rgba(${config.lib.stylix.colors.base00}bb)";
+            bordercolor = "rgba($mauveff) rgba($tealff) rgba($rosewaterff) 40deg";
+            blur = 1;
+            textpadding = 8;
+            textsize = 35;
+            bordersize = 2;
+            fullscreen_action = "maximize";
           };
         };
       };
     };
+  };
 }
