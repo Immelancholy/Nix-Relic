606dc0d9baf62d1b015802716b0f4843c0fc289b
Immelancholy <lenalowes0@gmail.com>
2026-05-03 23:16:10 +0100

    chore: update lanzaboote flake input to v1.0.0

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.3";
+      url = "github:nix-community/lanzaboote/v1.0.0";
 
       # Optional but recommended to limit the size of your system closure.
       inputs = {
