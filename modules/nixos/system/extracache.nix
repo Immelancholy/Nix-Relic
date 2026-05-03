63fb676b54ac58963ae1af16e2b66b31f28c835b
Immelancholy <lenalowes0@gmail.com>
2026-05-03 22:16:36 +0100

    chore(nix): update binary cache configuration
    - Add nixos-cuda cache settings.
    - Move yazi cache to standard substituters and keys.

--- modules/nixos/system/extracache.nix
+++ modules/nixos/system/extracache.nix
@@ -2,20 +2,22 @@
   nix.settings = {
     extra-trusted-public-keys = [
       "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
-      "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
     ];
     extra-trusted-substituters = [
-      "https://yazi.cachix.org"
       "https://cache.flox.dev"
     ];
     substituters = [
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
+      "https://cache.nixos-cuda.org"
+      "https://yazi.cachix.org"
     ];
 
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+      "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
     ];
   };
 }
