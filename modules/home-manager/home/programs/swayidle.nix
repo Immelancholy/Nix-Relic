84561afb1b9cd045aa84bed26ba5493621bcb2fd
Immelancholy <lenalowes0@gmail.com>
2026-05-31 23:49:40 +0100

    fix(swayidle): increase display power-off timeout
    Adjust the DPMS disable timeout from 10 seconds to 610 seconds to prevent premature screen suspension.

--- modules/home-manager/home/programs/swayidle.nix
+++ modules/home-manager/home/programs/swayidle.nix
@@ -14,7 +14,7 @@
         command = "${pkgs.swaylock}/bin/swaylock -fF";
       }
       {
-        timeout = 10;
+        timeout = 610;
         command = ''${inputs.nix-relic.inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl eval 'hl.dispatch(hl.dsp.dpms({ action = "disable" }))'  '';
         resumeCommand = ''${inputs.nix-relic.inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl eval 'hl.dispatch(hl.dsp.dpms({ action = "enable" }))'  '';
       }
