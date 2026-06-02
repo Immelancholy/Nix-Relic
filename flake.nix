2689b7499afcd5566b643b9a140001890e487414
Immelancholy <lenalowes0@gmail.com>
2026-06-02 22:15:32 +0100

    chore(deps): downgrade hyprquickframe to v1.0.2

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprquickframe = {
-      url = "github:Ronin-CK/HyprQuickFrame/v1.1.0";
+      url = "github:Ronin-CK/HyprQuickFrame/v1.0.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland.url = "github:hyprwm/Hyprland";
