bf497ae9efe70eac76259adb578b6b5ff4dffbaa
Immelancholy <lenalowes0@gmail.com>
2026-06-02 22:24:45 +0100

    chore(flake): update hyprquickframe to latest version
    Remove specific commit pin for hyprquickframe to track the latest upstream changes.

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprquickframe = {
-      url = "github:Ronin-CK/HyprQuickFrame/61fe0efda2db29cf2c4457f483735236d658631d";
+      url = "github:Ronin-CK/HyprQuickFrame";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland.url = "github:hyprwm/Hyprland";
