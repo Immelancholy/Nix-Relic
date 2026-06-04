f541370bb6b777772b446ca09fc502594a16b361
Immelancholy <lenalowes0@gmail.com>
2026-06-04 13:08:01 +0100

    refactor(hyprland): fix condition logic for live wallpaper
    Corrects the expression in mkIf to properly evaluate both
    cfg.liveWallpaper.enable and cfg.enable simultaneously.

--- modules/home-manager/wayland/windowManager/hyprland/useLiveWallpaper.nix
+++ modules/home-manager/wayland/windowManager/hyprland/useLiveWallpaper.nix
@@ -21,29 +21,27 @@ in
       description = "Path to animated background";
     };
   };
-  config =
-    mkIf cfg.liveWallpaper.enable
-    && cfg.enable {
-      home.packages = [
-        mpvpaper
-      ];
-      systemd.user.services.paper-change = {
-        Unit = {
-          Description = "Wallpaper Changer";
-          PartOf = [ "graphical-session.target" ];
-          Requires = [ "graphical-session.target" ];
-          After = [ "graphical-session.target" ];
-          ConditionEnvironment = "WAYLAND_DISPLAY";
-        };
-        Service = {
-          ExecStart = "/run/current-system/sw/bin/systemctl stop --user hyprpaper";
-          Type = "simple";
-          Slice = [ "session.slice" ];
-          Restart = "on-failure";
-        };
-        Install = {
-          WantedBy = [ "graphical-session.target" ];
-        };
+  config = mkIf (cfg.liveWallpaper.enable && cfg.enable) {
+    home.packages = [
+      mpvpaper
+    ];
+    systemd.user.services.paper-change = {
+      Unit = {
+        Description = "Wallpaper Changer";
+        PartOf = [ "graphical-session.target" ];
+        Requires = [ "graphical-session.target" ];
+        After = [ "graphical-session.target" ];
+        ConditionEnvironment = "WAYLAND_DISPLAY";
+      };
+      Service = {
+        ExecStart = "/run/current-system/sw/bin/systemctl stop --user hyprpaper";
+        Type = "simple";
+        Slice = [ "session.slice" ];
+        Restart = "on-failure";
+      };
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
       };
     };
+  };
 }
