77836c45d1c5221b10551d57e066dc2b0fa202c8
Immelancholy <lenalowes0@gmail.com>
2026-06-06 16:27:51 +0100

    fix: correct package reference for neo-color
    Update the override to target `pkgs.neo-color` instead of the incorrect `pkgs.nr.neo-color` path.

--- modules/home-manager/home/activationScripts/neo-color.nix
+++ modules/home-manager/home/activationScripts/neo-color.nix
@@ -8,7 +8,7 @@ let
   inherit (config.lib.stylix) colors;
   neo-color =
     with colors;
-    pkgs.nr.neo-color.override {
+    pkgs.neo-color.override {
       color1 = "${base0F}";
       color2 = "${base0D}";
       color3 = "${base0E}";
