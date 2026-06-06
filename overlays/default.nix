861e52540f9377fca3ba354b4eb6029dcf5c1003
Immelancholy <lenalowes0@gmail.com>
2026-06-06 15:40:29 +0100

    fix(overlay): correct import path for relicpkgs
    Update the path to load packages from the root directory instead of a subdirectory within overlays.

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,6 @@
 { self }:
 {
-  relicpkgs = final: prev: import ./packages { inherit self; } final.pkgs;
+  relicpkgs = final: prev: import ../packages { inherit self; } final.pkgs;
   stable = final: prev: {
     stable = import self.inputs.nixpkgs-stable {
       system = final.stdenv.hostPlatform.system;
