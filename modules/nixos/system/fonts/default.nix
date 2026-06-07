1e5d1d4a6f8997a7f08198251392279c12a2c0ff
Immelancholy <lenalowes0@gmail.com>
2026-06-08 00:57:00 +0100

    fonts: remove unecessary fonts

--- modules/nixos/system/fonts/default.nix
+++ modules/nixos/system/fonts/default.nix
@@ -5,15 +5,12 @@
     packages = with pkgs; [
       noto-fonts
       noto-fonts-cjk-sans
-      noto-fonts-color-emoji
       powerline
       powerline-symbols
-      nerd-fonts.jetbrains-mono
       nerd-fonts.caskaydia-cove
       nerd-fonts.fantasque-sans-mono
       nerd-fonts.symbols-only
       nerd-fonts.sauce-code-pro
-      nerd-fonts.noto
       nerd-fonts.fira-code
       font-awesome
       roboto
