47e2e1a4e6a5094d3c7f3eb01cc034dfdca38940
Immelancholy <lenalowes0@gmail.com>
2026-06-07 05:39:39 +0100

    Hyprland: Adjust binds and starts for new font + cava

--- packages/relic-cava.nix
+++ packages/relic-cava.nix
@@ -13,7 +13,7 @@ writeShellApplication {
 
   text = ''
     kitty @ set-spacing padding=0
-    kitty @ set-font-size 3
+    kitty @ set-font-size 1
     cava "$@"
     kitty @ set-spacing padding=default
     kitty @ set-font-size 10
