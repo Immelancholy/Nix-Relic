605a859d09d8e33e2176f60eede8a08d44d6c811
Immelancholy <lenalowes0@gmail.com>
2026-06-10 01:29:21 +0100

    waybar: bluetooth

--- modules/home-manager/home/programs/waybar/waybarsettings.nix
+++ modules/home-manager/home/programs/waybar/waybarsettings.nix
@@ -56,8 +56,8 @@ in
         "custom/l_end"
         "pulseaudio"
         "pulseaudio#mic"
-        "bluetooth"
         "network"
+        "bluetooth"
         "custom/r_end"
         "custom/l_end"
         "tray"
@@ -181,10 +181,10 @@ in
         '';
       };
       "bluetooth" = {
-        format-on = "󰂯";
-        format-off = "󰂲";
-        format-connected = "󰂯 {device_alias}";
-        format-connected-battery = "󰂯 {device_alias} {icon} {device_battery_percentage}%";
+        format-on = " 󰂯";
+        format-off = " 󰂲";
+        format-connected = " 󰂯 {device_alias}";
+        format-connected-battery = " 󰂯 {device_alias} {icon} {device_battery_percentage}%";
         tooltip-format = "{controller_alias}\t{controller_address}\n\n{num_connections} connected";
         tooltip-format-connected = "{controller_alias}\t{controller_address}\n\n{num_connections} connected\n\n{device_enumerate}";
         tooltip-format-enumerate-connected = "{device_alias}\t{device_address}";
