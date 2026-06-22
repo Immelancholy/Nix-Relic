1ef4204d77d5b1540838ffd1e0c4e20ae9fad5bb
Immelancholy <lenalowes0@gmail.com>
2026-06-22 01:37:47 +0100

    hyprland: remove my monitor config

--- template/hyprland/hardware.nix
+++ template/hyprland/hardware.nix
@@ -20,27 +20,5 @@
         key_press_enables_dpms = true,
       },
     })
-    hl.monitor({
-      output = "DP-1",
-      mode = "1920x1080@280",
-      position = "0x0",
-      scale = 1,
-      vrr = 1,
-      bitdepth = 10,
-      cm = "srgb",
-      supports_wide_color = 1,
-      supports_hdr = 1,
-      sdr_min_luminance = 0.005,
-      sdr_max_luminance = 300,
-      min_luminance = 0,
-      max_luminance = 300,
-      max_avg_luminance = 300,
-    })
-    hl.monitor({
-      output = "HDMI-A-1",
-      mode = "2569x1440@60",
-      position = "1920x0",
-      scale = 1,
-    })
   '';
 }
